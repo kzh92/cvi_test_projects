cc7c16beabd2fee585378684607dce942d296441
kzh <kevin@outlook.com>
2023-04-11 20:12:00 +0800

    - add header info for upgrade image on USB upgrade.  need to extract generated/images.zip to generated/images

---
+++ solutions/usb_cam/make_upgrade.sh
@@ -0,0 +1,7 @@
+rm -rf ./generated/upgrade
+cp -rf ./generated/images ./generated/upgrade
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/fip_fsbl.bin ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/yoc.bin ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/boot ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/boot0 ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/imtb ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
