a4abee9c6fd16775031b6913fdf4bb3fe8e52039
kzh <kevin@outlook.com>
2023-05-12 08:12:17 +0800

    - changed make_upgrade.sh

--- solutions/usb_cam/make_upgrade.sh
+++ solutions/usb_cam/make_upgrade.sh
@@ -1,11 +1,12 @@
 rm -rf ./generated/upgrade
 cp -rf ./generated/images ./generated/upgrade
-#python3 ../../boards/tools/common/raw2cimg.py ./generated/images/fip_fsbl.bin ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
-#python3 ../../boards/tools/common/raw2cimg.py ./generated/images/yoc.bin ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
-#python3 ../../boards/tools/common/raw2cimg.py ./generated/images/boot ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/fip_fsbl.bin ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/yoc.bin ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/boot ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
 python3 ../../boards/tools/common/raw2cimg.py ./generated/images/boot0 ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
-#python3 ../../boards/tools/common/raw2cimg.py ./generated/images/imtb ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
-#python3 ../../boards/tools/common/raw2cimg.py ./generated/images/misc.bin ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
-#python3 ../../boards/tools/common/raw2cimg.py ./generated/images/partwx.bin ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
-#python3 ../../boards/tools/common/raw2cimg.py ./generated/images/pusr1.bin ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
-#python3 ../../boards/tools/common/raw2cimg.py ./generated/images/pusr2.bin ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/imtb ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/misc ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/pwx ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/pusr1 ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/pusr2 ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
+python3 ../../boards/tools/common/raw2cimg.py ./generated/images/pst ./generated/upgrade ./generated/upgrade/partition_alios_spinor.xml
