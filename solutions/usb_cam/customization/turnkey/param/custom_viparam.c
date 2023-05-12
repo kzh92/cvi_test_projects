58a82e879e52e99867b536cf41ac76a3b41df276
kzh <kevin@outlook.com>
2023-05-12 18:00:05 +0800

    - use dma_buf in custom_vi

--- solutions/usb_cam/customization/turnkey/param/custom_viparam.c
+++ solutions/usb_cam/customization/turnkey/param/custom_viparam.c
@@ -6,6 +6,10 @@
  *   ....
  */
 #include "custom_param.h"
+#if 1
+void * g_ViDmaBuf = NULL;
+unsigned int g_ViDmaBufSize = 13 * 1024 * 1024;
+#endif
 PARAM_CLASSDEFINE(PARAM_SNS_CFG_S,SENSORCFG,CTX,Sensor)[] = {
     {
         .enSnsType = CONFIG_SNS0_TYPE,
@@ -29,13 +33,28 @@ PARAM_CLASSDEFINE(PARAM_ISP_CFG_S,ISPCFG,CTX,ISP)[] = {
     },
 };
 
+PARAM_CLASSDEFINE(PARAM_DEV_CFG_S,VIDEVCFG,CTX,VI)[] = {
+    {
+        .pViDmaBuf = NULL,
+        .u32ViDmaBufSize = 0,
+    }
+};
+
 PARAM_VI_CFG_S g_stViCtx = {
     .u32WorkSnsCnt = 1,
     .pstSensorCfg = PARAM_CLASS(SENSORCFG,CTX,Sensor),
     .pstIspCfg = PARAM_CLASS(ISPCFG,CTX,ISP),
+    .pstDevInfo = PARAM_CLASS(VIDEVCFG,CTX,VI)
 };
 
 PARAM_VI_CFG_S * PARAM_GET_VI_CFG(void) {
+#if 1
+    if(g_ViDmaBuf == NULL) {
+        g_ViDmaBuf = (void *)malloc(g_ViDmaBufSize);
+    }
+    g_stViCtx.pstDevInfo[0].pViDmaBuf = g_ViDmaBuf;
+    g_stViCtx.pstDevInfo[0].u32ViDmaBufSize = g_ViDmaBufSize;
+#endif
     return &g_stViCtx;
 }
 
