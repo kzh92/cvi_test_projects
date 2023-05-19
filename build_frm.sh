3146f2f0a600c1367e736f968892e6123ac9148f
kzh <kevin@outlook.com>
2023-05-19 08:32:56 +0800

    - build script: stop building on error

--- build_frm.sh
+++ build_frm.sh
@@ -1,6 +1,21 @@
+if [ ! -f ".yoc" ]; then
+    echo "Initialisng yoc..."
+    yoc init
+fi
 cd pack
 cmake .
+rm -f ./config_app
 make
-./config_app
-cd ../
-make -j7 usb_cam PROJECT=turnkey_180xb
+if [ ! -f "./config_app" ]; then
+	echo "Failed to compile pack app."
+	cd ../
+else
+	./config_app
+	cd ../
+	make -j4 usb_cam PROJECT=turnkey_180xb
+	echo "Compressing prim..."
+	rm -f lz4 ./solutions/usb_cam/generated/images/prim.lz4
+	lz4 ./solutions/usb_cam/generated/images/prim
+	mv ./solutions/usb_cam/generated/images/prim.lz4 ./solutions/usb_cam/generated/images/prim
+fi
+
