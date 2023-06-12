7abdfef9904fdedc7149af02b5f4e3d8fd36ab47
kzh <kevin@outlook.com>
2023-06-12 09:38:16 +0800

    - 화상통화모듈련동에서 얼굴인식모듈이 폭주되는 경우가 있어 수정하였음.  길이마당을 0으로 설정하였음.

--- components/cherryusb/core/usbd_core.c
+++ components/cherryusb/core/usbd_core.c
@@ -871,6 +871,7 @@ static bool usbd_setup_request_handler(struct usb_setup_packet *setup, uint8_t *
             if (usbd_class_request_handler(setup, data, len) < 0) {
                 USB_LOG_ERR("class request error\r\n");
                 usbd_print_setup(setup);
+                *len = 0;
                 //return false;
             }
             break;
