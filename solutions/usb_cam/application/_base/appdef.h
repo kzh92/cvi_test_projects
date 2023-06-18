a919e8aabdc1115e8664e22c3f698fef07146a04
darkhorse <darkhorse215@outlook.com>
2023-06-18 16:51:19 +0800

    9.43.1_D2 중산푸씬대상

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -476,8 +476,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DAS3M_PUXIN_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "9.43.0.4_D2"
-#define DEVICE_FIRMWARE_VERSION_INNER       "9.43.0.4_D2"
+#define DEVICE_FIRMWARE_VERSION             "9.43.1_D2"
+#define DEVICE_FIRMWARE_VERSION_INNER       "9.43.1_D2"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D20
