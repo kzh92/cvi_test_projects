5b7b87bf079deb7c25ed5edadfe5c8c2275ea1d4
kzh <kevin@outlook.com>
2023-06-25 16:33:40 +0800

    - 9.14.3.8_D2

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -445,8 +445,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DAS3M_D20_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "9.14.3.7_D2"
-#define DEVICE_FIRMWARE_VERSION_INNER       "9.14.3.7_D2"
+#define DEVICE_FIRMWARE_VERSION             "9.14.3.8_D2"
+#define DEVICE_FIRMWARE_VERSION_INNER       "9.14.3.8_D2"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D20
