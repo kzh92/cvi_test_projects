93776374b1f1f0255ef265e93e0a21e1bc62051e
darkhorse <darkhorse215@outlook.com>
2023-09-07 16:04:28 +0800

    3.45.1_D

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -381,7 +381,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_LS35_LH_UAC                   306
 #define FRM_DBS3M_AIPAI_TOYA_UAC                307
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AIPAI_TOYA_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_HUANGLI_NEW_UAC
 
 //---------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS20_DEFAULT)
@@ -943,8 +943,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_HUANGLI_NEW_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.45.0_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.45.0_D"
+#define DEVICE_FIRMWARE_VERSION             "3.45.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.45.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
