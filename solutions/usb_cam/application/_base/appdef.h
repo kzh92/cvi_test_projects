2c711e9786028c2d8c7415d22c7a8fd4978f6c7e
darkhorse <darkhorse215@outlook.com>
2023-09-08 10:14:11 +0800

    3.15.0_D 애박도아방안화상대화 + 범해삼강조종기판 혼합등록방식

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -382,8 +382,9 @@ enum E_Baud_Rate
 #define FRM_DBS3M_TONGXIN_PROTO                 305
 #define FRM_DBS3M_LS35_LH_UAC                   306
 #define FRM_DBS3M_AIPAI_TOYA_UAC                307
+#define FRM_DBS3M_AIPAI_TOYA_SANJIANG_MODE      308
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_HUANGLI_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AIPAI_TOYA_SANJIANG_MODE
 
 //---------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS20_DEFAULT)
@@ -788,6 +789,47 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
 
+//----------------------------------------------------------
+
+#elif (FRM_PRODUCT_TYPE == FRM_DBS3M_AIPAI_TOYA_SANJIANG_MODE)
+
+#define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
+#define DEVICE_FIRMWARE_VERSION             "3.15.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.15.0_D"
+
+#undef DEFAULT_CHIP_TYPE
+#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
+#undef DEFAULT_PROTO_ENC_MODE
+#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
+#undef UVC_RES_DEFINE
+#define UVC_RES_DEFINE                      {1, 640, 360, 30, 0},
+#undef ENGINE_USE_TWO_CAM
+#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
+#undef USE_3M_MODE
+#define USE_3M_MODE                         1
+#undef USE_UAC_MODE
+#define USE_UAC_MODE                        1
+#undef DEFAULT_BOARD_TYPE
+#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
+#undef DEFAULT_CAM_MIPI_TYPE
+#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
+// #undef CAM_ROTATION_MODE
+// #define CAM_ROTATION_MODE                   CAM_RM_180DEGREE
+#undef USE_VDBTASK
+#define USE_VDBTASK                         1
+#undef USE_SANJIANG3_MODE
+#define USE_SANJIANG3_MODE                  1
+#undef N_MAX_HAND_NUM
+#define N_MAX_HAND_NUM                      100
+#undef USE_WHITE_LED
+#define USE_WHITE_LED                       1
+#undef UVC_MJPEG_BITRATE
+#define UVC_MJPEG_BITRATE                   2048
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
+#undef ENROLL_FACE_HAND_MODE
+#define ENROLL_FACE_HAND_MODE               ENROLL_FACE_HAND_MIX
+                                            
 //----------------------------------------------------------
 
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_HONGLI_MODE)
