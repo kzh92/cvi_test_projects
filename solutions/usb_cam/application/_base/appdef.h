e989a71aff0f6961da5dd8dfe947435a65baf089
darkhorse <darkhorse215@outlook.com>
2023-09-08 09:51:13 +0800

    3.8.1_D 중산황력대상 - USB완전성검사기능이 동작하지 않게 하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -383,7 +383,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_LS35_LH_UAC                   306
 #define FRM_DBS3M_AIPAI_TOYA_UAC                307
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_HUANGLI_NEW_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_HUANGLI_UAC
 
 //---------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS20_DEFAULT)
@@ -680,8 +680,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_HUANGLI_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.8.0_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.8.0_D"
+#define DEVICE_FIRMWARE_VERSION             "3.8.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.8.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -712,6 +712,8 @@ enum E_Baud_Rate
 #define USE_WHITE_LED                       1
 #undef USE_SHENAO_HAND
 #define USE_SHENAO_HAND                     1
+#undef USE_USB_CHECKFIRM_MODE
+#define USE_USB_CHECKFIRM_MODE              0
 
 //----------------------------------------------------------
 
