db34fec79c9417d66c6291929f4720e462be6f22
kzh <kevin@outlook.com>
2023-09-13 11:14:28 +0800

    - 3.14.5.2_D  색카메라현시효과 v2.1.4

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -799,8 +799,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_DEFAULT)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.14.5.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.14.5.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.14.5.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.14.5.2_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -827,7 +827,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v2
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v4
 
 //----------------------------------------------------------
 
