701f160a83cc18d23f7c94bc99fe64f4072c9341
darkhorse <darkhorse215@outlook.com>
2023-09-19 11:12:40 +0800

    3.7.1_D: 절강아길사 FRM3M5.0DBSSW(3.01) 1차생산 창시(웅매방안)화상대화모듈

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -689,8 +689,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_AJISHI_CHANGSI_MODE)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.7.0.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.7.0.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.7.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.7.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
