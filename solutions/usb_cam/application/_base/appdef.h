119b22e5f254a6208307212c1290c669fb00ecbe
kzh <kevin@outlook.com>
2023-09-21 23:26:27 +0800

    - 3.45.3.1_D  화상대화로 출력하는 색카메라화상용량을 늘였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -1112,15 +1112,15 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_HUANGLI_NEW_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.45.3_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.45.3_D"
+#define DEVICE_FIRMWARE_VERSION             "3.45.3.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.45.3.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
 #undef DEFAULT_PROTO_ENC_MODE
 #define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
 #undef UVC_RES_DEFINE
-#define UVC_RES_DEFINE                      {1, 480, 320, 30, 0, 4096},
+#define UVC_RES_DEFINE                      {1, 480, 320, 30, 0, 8192},
 #undef ENGINE_USE_TWO_CAM
 #define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
 #undef USE_3M_MODE
