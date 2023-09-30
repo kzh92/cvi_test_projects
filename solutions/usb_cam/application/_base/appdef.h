750f90fc41f2c04e4911bd9b6e20b2748f4ef6f2
kci <kci94@outlook.com>
2023-10-01 00:16:08 +0800

    3.49.0_D: 중산대경 동흔조종기판 + 애박화상대화모듈(뒤카메라결합방안)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -402,8 +402,9 @@ enum E_Baud_Rate
 #define FRM_DBS3M_AJISHI_CHANGSI_MODE           311
 #define FRM_DBS3M_FANGKUAI_MODE                 312
 #define FRM_DBS3M_PUXIN2                        313
+#define FRM_DBS3M_DUAL_CAM_AIPAI                314
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_PUXIN2
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_DUAL_CAM_AIPAI
 
 //---------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS20_DEFAULT)
@@ -1248,6 +1249,41 @@ enum E_Baud_Rate
 #define USE_USB_CHECKFIRM_MODE              0
 #undef DEFAULT_ISP_BIN_VER
 #define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_22v0
+
+#elif (FRM_PRODUCT_TYPE == FRM_DBS3M_DUAL_CAM_AIPAI)
+
+#define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
+#define DEVICE_FIRMWARE_VERSION             "3.49.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.49.0_D"
+
+#undef DEFAULT_CHIP_TYPE
+#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
+#undef DEFAULT_PROTO_ENC_MODE
+#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
+#undef UVC_RES_DEFINE
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0},
+#undef ENGINE_USE_TWO_CAM
+#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
+#undef USE_3M_MODE
+#define USE_3M_MODE                         1
+#undef USE_UAC_MODE
+#define USE_UAC_MODE                        1
+#undef DEFAULT_BOARD_TYPE
+#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
+#undef DEFAULT_CAM_MIPI_TYPE
+#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
+#undef USE_VDBTASK
+#define USE_VDBTASK                         1
+#undef USE_SANJIANG3_MODE
+#define USE_SANJIANG3_MODE                  1
+#undef N_MAX_HAND_NUM
+#define N_MAX_HAND_NUM                      100
+#undef USE_WHITE_LED
+#define USE_WHITE_LED                       1
+#undef DEFAULT_ISP_BIN_VER
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
 
 //----------------------------------------------------------
 
