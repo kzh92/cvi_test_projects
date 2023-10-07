e39ce7eee444457c1b0198c10ae1faab4beb2e27
kzh <kevin@outlook.com>
2023-10-07 15:01:16 +0800

    - 3.9.0_D  절강호흥  중련7916화상대화, 중련화상대화(중산왕력대상용)을 결합함  화상대화방향을 180도 회전하였음.  람성화상대화도 가능함

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -403,8 +403,9 @@ enum E_Baud_Rate
 #define FRM_DBS3M_FANGKUAI_MODE                 312
 #define FRM_DBS3M_PUXIN2                        313
 #define FRM_DBS3M_DUAL_CAM_AIPAI                314
+#define FRM_DBS3M_LANCENS_UAC                   315
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_DUAL_CAM_AIPAI
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LANCENS_UAC
 
 //---------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS20_DEFAULT)
@@ -824,6 +825,43 @@ enum E_Baud_Rate
 #define USE_SHENAO_HAND                     1
 #undef USE_USB_CHECKFIRM_MODE
 #define USE_USB_CHECKFIRM_MODE              0
+
+//----------------------------------------------------------
+#elif (FRM_PRODUCT_TYPE == FRM_DBS3M_LANCENS_UAC)
+
+#define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
+#define DEVICE_FIRMWARE_VERSION             "3.9.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.9.0_D"
+
+#undef DEFAULT_CHIP_TYPE
+#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
+#undef DEFAULT_PROTO_ENC_MODE
+#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
+#undef UVC_RES_DEFINE
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0, 10240}, \
+                                            {2, 640, 480, 30, 0, 4096},
+#undef DEFAULT_UVC_DIR
+#define DEFAULT_UVC_DIR                     UVC_ROTATION_90
+#undef ENGINE_USE_TWO_CAM
+#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
+#undef USE_3M_MODE
+#define USE_3M_MODE                         1
+#undef USE_UAC_MODE
+#define USE_UAC_MODE                        1
+#undef DEFAULT_BOARD_TYPE
+#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
+#undef DEFAULT_CAM_MIPI_TYPE
+#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
+#undef USE_VDBTASK
+#define USE_VDBTASK                         1
+#undef USE_SANJIANG3_MODE
+#define USE_SANJIANG3_MODE                  1
+#undef N_MAX_HAND_NUM
+#define N_MAX_HAND_NUM                      100
+#undef USE_WHITE_LED
+#define USE_WHITE_LED                       1
+#undef DEFAULT_ISP_BIN_VER
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
 
 //----------------------------------------------------------
 
