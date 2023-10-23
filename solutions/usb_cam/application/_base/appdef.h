a3b114bc69d6a211878350038128273a451111cf
kci <kci94@outlook.com>
2023-10-23 15:18:12 +0800

    - 3.3.1_D  중산푸씬대상용  애박화상대화모듈을 결합할때 음성통화가 안되는 문제를 대책하였음.  (호위화상대화와 애박화상대화모듈을 동시에 리용하는데 애박화상대화모듈을 결합하면 음성통화가 안되는 문제가 있었음.)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -409,7 +409,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_LIWEN_UAC                     318
 #define FRM_DBS3M_TOYO_UAC                      319
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_FANHAI_MODE
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_PUXIN2
 
 //---------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS20_DEFAULT)
@@ -706,8 +706,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_PUXIN2)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.3.0_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.3.0_D"
+#define DEVICE_FIRMWARE_VERSION             "3.3.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.3.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -741,6 +741,8 @@ enum E_Baud_Rate
 #define UAC_SPEAKER_VOL                     6 // 0 ~ 32
 #undef USE_USB_CHECKFIRM_MODE
 #define USE_USB_CHECKFIRM_MODE              0
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_AJISHI_CHANGSI_MODE)
