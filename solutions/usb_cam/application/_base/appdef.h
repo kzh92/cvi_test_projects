1afd47fb7f54fc148662f890e0a550bca84e3c26
kci <kci94@outlook.com>
2023-10-26 10:32:24 +0800

    - 3.50.2_D  중산금강랑  화상이 가리우는 문제가 있어 시창구에 맞게 화상을 잘랐음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -1500,8 +1500,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_OKEDA_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.50.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.50.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.50.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.50.2_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1539,6 +1539,8 @@ enum E_Baud_Rate
 #define SPECIFIC_LOG_PRINT                  1
 #undef UAC_SPK_EP
 #define UAC_SPK_EP                          0x83
+#undef UVC_CROP_RESIZE
+#define UVC_CROP_RESIZE                     0.88
 
 //----------------------------------------------------------
 
