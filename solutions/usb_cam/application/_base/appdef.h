3ed2b264e3704e9540507639afdf4c099a340525
kzh <kevin@outlook.com>
2023-11-14 09:30:14 +0800

    - 3.18.3_D  중산빈예대상 범해삼강조종기판+범해삼강화상대화 음성통화 하고나면 화상이 굳는 문제가 있어 UAC Endpoint를 0x83으로 설정하였음. 로그출력은 큰 영향을 주지 않는다고 보지만 그대로 두었음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -417,7 +417,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_KEXIONG_UAC                   322
 #define FRM_DBS3M_HAND_PRIO_UAC                 323
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_DEFAULT
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_FANHAI_MODE
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DAS3M_LS35_LH_UAC)
@@ -911,8 +911,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_FANHAI_MODE)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.18.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.18.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.18.3_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.18.3_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -942,6 +942,10 @@ enum E_Baud_Rate
 #define ENROLL_FACE_HAND_MODE               ENROLL_FACE_HAND_MIX
 #undef DEFAULT_ISP_BIN_VER
 #define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
+#undef UAC_SPK_EP
+#define UAC_SPK_EP                          0x83
+#undef SPECIFIC_LOG_PRINT
+#define SPECIFIC_LOG_PRINT                  1
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_FANGKUAI_MODE)
