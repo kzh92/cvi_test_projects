a88176f1e4219d27db00704fd6737319224b2c95
kzh <kevin@outlook.com>
2023-11-16 17:21:19 +0800

    - 3.44.0.2_D  UAC Speaker Endpoint번호를 0x83으로 수정하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -1308,8 +1308,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_XIONGMAI_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.44.0.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.44.0.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.44.0.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.44.0.2_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1349,6 +1349,8 @@ enum E_Baud_Rate
 #define USE_USB_EP_ERR_FIX_MODE             1
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
+#undef UAC_SPK_EP
+#define UAC_SPK_EP                          0x83
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_HUANGLI_NEW_UAC)
