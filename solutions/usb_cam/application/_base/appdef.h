98fa0cf60a0b893c33577c91ee98ea53d0511315
kzh <kevin@outlook.com>
2023-11-22 00:45:20 +0800

    - 3.32.4_D  람성화상대화모듈결합(180도회전) 적외선절환하는 턱값판단방법을 수정하였음. 밝은환경에서 얼굴인식에 색카메라화상을 리용하고 어두운 환경에서 색카메라화상을 리용하지 않게 하였음. 음성통화도중에 화상대화중지되는 문제의 례외처리로그출력하게 하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -1167,8 +1167,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_LS35_LH_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.32.3_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.32.3_D"
+#define DEVICE_FIRMWARE_VERSION             "3.32.4_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.32.4_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
