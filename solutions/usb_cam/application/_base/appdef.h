e8247c8de16edbe2733c49ef40530594b8f3e74c
kzh <kevin@outlook.com>
2023-11-28 19:47:39 +0800

    - 3.4.0_D  절강가사방대상  심전지성(얼굴, 손바닥혼합등록방식)조종기판과 절강호위화상대화모듈  3.43.5_D판본에서 시작하였음.  백색레드 켜고 화상대화에 색카메라화상을 항상 출력.  낮에만 색카메라를 인식에 리용.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -425,8 +425,9 @@ enum E_Baud_Rate
 #define FRM_DBS3M_HAND_PRIO_UAC                 323
 #define FRM_DBS3M_7916_UAC                      324
 #define FRM_DBS3M_YIHE_UAC                      325
+#define FRM_DBS3M_JIASHIBANG_UAC                326
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AJISHI_CHANGSI_MODE
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_JIASHIBANG_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -552,6 +553,52 @@ enum E_Baud_Rate
 #define USE_USB_EP_ERR_FIX_MODE             1
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
+
+//----------------------------------------------------------
+#elif (FRM_PRODUCT_TYPE == FRM_DBS3M_JIASHIBANG_UAC)
+
+#define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
+#define DEVICE_FIRMWARE_VERSION             "3.4.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.4.0_D"
+
+#undef DEFAULT_CHIP_TYPE
+#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
+#undef DEFAULT_PROTO_ENC_MODE
+#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
+#undef UVC_RES_DEFINE
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0, 20480}, \
+                                            {2, 864, 480, 30, 0, 10240},\
+                                            {3, 800, 480, 30, 0, 10240},\
+                                            {4, 640, 480, 30, 0, 8192},\
+                                            {5, 480, 320, 30, 0, 4096},\
+                                            {6, 320, 240, 30, 0, 4096}
+#undef ENGINE_USE_TWO_CAM
+#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_SEMI
+#undef USE_UAC_MODE
+#define USE_UAC_MODE                        1
+#undef DEFAULT_BOARD_TYPE
+#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
+#undef DEFAULT_CAM_MIPI_TYPE
+#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
+#undef USE_VDBTASK
+#define USE_VDBTASK                         1
+#undef USE_SANJIANG3_MODE
+#define USE_SANJIANG3_MODE                  1
+#undef N_MAX_HAND_NUM
+#define N_MAX_HAND_NUM                      100
+#undef USE_WHITE_LED
+#define USE_WHITE_LED                       1
+#undef ENROLL_FACE_HAND_MODE
+#define ENROLL_FACE_HAND_MODE               ENROLL_FACE_HAND_MIX
+#undef DEFAULT_ISP_BIN_VER
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
+#undef SPECIFIC_LOG_PRINT
+#define SPECIFIC_LOG_PRINT                  1
+#define UVC_CLR2IR_THR4ISP                  (-250) //threshold value for turning white led on.
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_KEXIONG_UAC)
