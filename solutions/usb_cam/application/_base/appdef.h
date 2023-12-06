a9bca1c78c41533a2919076926ad841bbfe5a109
kzh <kevin@outlook.com>
2023-12-06 14:38:41 +0800

    - 3.44.2.1_D  웅매화상대화련동  UVC_CLR2IR_THR4ISP = (-200)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -439,7 +439,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_BINRUI10IN_UAC                327
 #define FRM_DBS3M_XINNENG_H264                  328
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_JIGAO_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XIONGMAI_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1532,8 +1532,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_XIONGMAI_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.44.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.44.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.44.2.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.44.2.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1579,7 +1579,7 @@ enum E_Baud_Rate
 #define SPECIFIC_LOG_PRINT                  1
 #undef UAC_SPK_EP
 #define UAC_SPK_EP                          0x83
-#define UVC_CLR2IR_THR4ISP                  (-250)
+#define UVC_CLR2IR_THR4ISP                  (-200)
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_HUANGLI_NEW_UAC)
