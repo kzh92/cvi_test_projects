be115d46b73f10858e4695a4eddcf2dc4eb45bf8
kzh <kevin@outlook.com>
2023-12-11 19:40:08 +0800

    - 3.44.7_D  절강박극, 웅매화상대화모듈련동

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -444,7 +444,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_BINRUI10IN_UAC                327
 #define FRM_DBS3M_XINNENG_H264                  328
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_TONGXIN_PROTO
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XIONGMAI_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1553,8 +1553,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_XIONGMAI_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.44.6_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.44.6_D"
+#define DEVICE_FIRMWARE_VERSION             "3.44.7_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.44.7_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
