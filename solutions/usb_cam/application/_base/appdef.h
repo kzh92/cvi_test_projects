ee4c76cf897d78595f31958445a05b6caba0396e
kzh <kevin@outlook.com>
2023-12-11 20:59:40 +0800

    - 3.7.4_D  절강아길사, 창시웅매화상대화모듈련동  혼합등록방식을 막았음.  백색레드리용하게 하고 인증할 때 백색레드 켜는 턱값을 3.14.10_D판본보다 낮추었음(엔진조에서 시험하고 낮게 설정하였음).

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -444,7 +444,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_BINRUI10IN_UAC                327
 #define FRM_DBS3M_XINNENG_H264                  328
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XIONGMAI_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AJISHI_CHANGSI_MODE
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -712,8 +712,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_AJISHI_CHANGSI_MODE)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.7.3_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.7.3_D"
+#define DEVICE_FIRMWARE_VERSION             "3.7.4_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.7.4_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -730,7 +730,7 @@ enum E_Baud_Rate
 #undef UAC_AUDALGO_USE 
 #define UAC_AUDALGO_USE                       1
 #undef USE_3M_MODE
-#define USE_3M_MODE                         U3M_SEMI
+#define USE_3M_MODE                         1
 #undef USE_UAC_MODE
 #define USE_UAC_MODE                        1
 #undef UAC_SPEAKER_VOL
@@ -746,7 +746,7 @@ enum E_Baud_Rate
 #undef N_MAX_HAND_NUM
 #define N_MAX_HAND_NUM                      100
 #undef USE_WHITE_LED
-#define USE_WHITE_LED                       0
+#define USE_WHITE_LED                       1
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
 #undef DEFAULT_ISP_BIN_VER
@@ -755,8 +755,8 @@ enum E_Baud_Rate
 #define SPECIFIC_LOG_PRINT                  1
 #undef UAC_SPK_EP
 #define UAC_SPK_EP                          0x83
-#undef ENROLL_FACE_HAND_MODE
-#define ENROLL_FACE_HAND_MODE               ENROLL_FACE_HAND_MIX
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#define UVC_CLR2IR_THR4ENGINE               (-30)
 
 //----------------------------------------------------------
 
