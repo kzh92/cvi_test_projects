ddb365458ac9c7aba022efa82a97718a2d2eb80d
kzh <kevin@outlook.com>
2023-12-13 14:17:05 +0800

    - 3.14.11_D  백색레드켜는 턱값을 3.7.4_D판본에 맞추었음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -444,7 +444,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_BINRUI10IN_UAC                327
 #define FRM_DBS3M_XINNENG_H264                  328
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_FANGKUAI_MODE
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_DEFAULT
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -957,8 +957,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_DEFAULT)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.14.10_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.14.10_D"
+#define DEVICE_FIRMWARE_VERSION             "3.14.11_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.14.11_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -992,7 +992,7 @@ enum E_Baud_Rate
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
 #define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
-#define UVC_CLR2IR_THR4ENGINE               (-100)
+#define UVC_CLR2IR_THR4ENGINE               (-30)
 
 //----------------------------------------------------------
 
