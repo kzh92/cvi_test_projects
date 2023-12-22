e8972acc683aead1d47b9535d166cf1af15723a2
darkhorse <darkhorse215@outlook.com>
2023-12-22 14:21:40 +0800

    - 3.21.5.3_D  하문마싸로크대상  범해삼강조종기판 + 방괴화상대화모듈 UVC출력해상도를 320*480으로 변경하고 마이크감도가 조금 높은(3.21.4_D와 3.21.5_D사이 절반) 펌웨어.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -1291,8 +1291,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_FANGKUAI_MODE)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.21.5.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.21.5.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.21.5.3_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.21.5.3_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1340,8 +1340,8 @@ enum E_Baud_Rate
 #define UVC_CLR2IR_THR4ENGINE               (-30)
 #undef UAC_AUDALGO_USE
 #define UAC_AUDALGO_USE                     1
-// #undef UAC_MIC_VOL
-// #define UAC_MIC_VOL                         24
+#undef UAC_MIC_VOL
+#define UAC_MIC_VOL                         18
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_XIJIN_UAC)
