3b0a57ecd8d9da096e5d99b6927eb27b4baaaef4
kzh <kevin@outlook.com>
2024-01-02 16:05:38 +0800

    - 3.18.4_D  중산빈예대상  범해삼강조종기판+범해삼강화상대화  800x480해상도를 추가하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -461,7 +461,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_TX_PROTO_IR                   333
 #define FRM_DBS3M_TX2_UAC                       334
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XINNENG_H264
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_FANHAI_MODE
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1166,15 +1166,18 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_FANHAI_MODE)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.18.3_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.18.3_D"
+#define DEVICE_FIRMWARE_VERSION             "3.18.4_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.18.4_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
 #undef DEFAULT_PROTO_ENC_MODE
 #define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
 #undef UVC_RES_DEFINE
-#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0},
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
+                                            {2, 864, 480, 30, 0, 10240}, \
+                                            {3, 800, 480, 30, 0, 10240}, \
+                                            {4, 640, 480, 30, 0, 6144},
 #undef ENGINE_USE_TWO_CAM
 #define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
 #undef USE_3M_MODE
