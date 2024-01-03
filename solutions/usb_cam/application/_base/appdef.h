38cde51aa4a3d3710dbfef384d6b071f69edc9a7
kzh <kevin@outlook.com>
2024-01-03 17:04:26 +0800

    - 3.25.0.1_D  중산세기  3.50.4_D, 3.14.12_D에서 시작하여 오케다화상대화모듈과 련동하게 하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -460,8 +460,9 @@ enum E_Baud_Rate
 #define FRM_DBS3M_JINJIAN_UAC                   332
 #define FRM_DBS3M_TX_PROTO_IR                   333
 #define FRM_DBS3M_TX2_UAC                       334
+#define FRM_DBS3M_OKEDA2_UAC                    335
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_FANHAI_MODE
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_OKEDA2_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1486,6 +1487,52 @@ enum E_Baud_Rate
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
 #define UVC_CLR2IR_THR4ISP                  (-200)
+
+//----------------------------------------------------------
+#elif (FRM_PRODUCT_TYPE == FRM_DBS3M_OKEDA2_UAC)
+
+#define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
+#define DEVICE_FIRMWARE_VERSION             "3.25.0.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.25.0.1_D"
+
+#undef DEFAULT_CHIP_TYPE
+#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
+#undef DEFAULT_PROTO_ENC_MODE
+#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
+#undef UVC_RES_DEFINE
+#define UVC_RES_DEFINE                      {1, 1280, 720, 15, 0, 10240}, \
+                                            {2, 864, 480, 15, 0, 8192}, \
+                                            {3, 800, 480, 15, 0, 8192}, \
+                                            {4, 480, 320, 15, 0, 4096}, \
+                                            {5, 656, 368, 15, 0, 4096},
+#undef ENGINE_USE_TWO_CAM
+#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
+#undef USE_3M_MODE
+#define USE_3M_MODE                         1
+#undef USE_UAC_MODE
+#define USE_UAC_MODE                        1
+#undef DEFAULT_BOARD_TYPE
+#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
+#undef DEFAULT_CAM_MIPI_TYPE
+#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
+#undef USE_VDBTASK
+#define USE_VDBTASK                         1
+#undef USE_SANJIANG3_MODE
+#define USE_SANJIANG3_MODE                  1
+#undef N_MAX_HAND_NUM
+#define N_MAX_HAND_NUM                      100
+#undef USE_WHITE_LED
+#define USE_WHITE_LED                       1
+#undef DEFAULT_ISP_BIN_VER
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v0
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
+#undef SPECIFIC_LOG_PRINT
+#define SPECIFIC_LOG_PRINT                  1
+#undef UAC_SPK_EP
+#define UAC_SPK_EP                          0x83
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#define UVC_CLR2IR_THR4ENGINE               (-30)
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_TONGXIN_PROTO)
