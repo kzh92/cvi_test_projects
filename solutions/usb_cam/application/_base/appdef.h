3eedf83a8c29a409aa5f009f04d9ce6ed6de4191
kzh <kevin@outlook.com>
2024-01-05 17:54:00 +0800

    - 3.18.5_D  중산빈예대상  범해삼강조종기판+범해삼강화상대화  색카메라화상현시에서 잡음을 없앤 판본

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -462,7 +462,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_TX2_UAC                       334
 #define FRM_DBS3M_OKEDA2_UAC                    335
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_OKEDA2_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_FANHAI_MODE
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1167,8 +1167,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_FANHAI_MODE)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.18.4_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.18.4_D"
+#define DEVICE_FIRMWARE_VERSION             "3.18.5_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.18.5_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1200,7 +1200,7 @@ enum E_Baud_Rate
 #undef ENROLL_FACE_HAND_MODE
 #define ENROLL_FACE_HAND_MODE               ENROLL_FACE_HAND_MIX
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v0
 #undef UAC_SPK_EP
 #define UAC_SPK_EP                          0x83
 #undef SPECIFIC_LOG_PRINT
