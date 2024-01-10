79d44eec165d97f75f6de3d98f09cbf144eef915
kzh <kevin@outlook.com>
2024-01-10 10:08:27 +0800

    - 3.28.1_D  절강가사방대상  색카메라화상에 잡음이 적은 현시효과를 리용하게 하였음(v2.1.0.0).

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -465,7 +465,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_OKEDA2_UAC                    335
 #define FRM_DBS3M_TONGXIN_UVC                   336
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_OKEDA2_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_IR_DEFAULT
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1606,8 +1606,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_IR_DEFAULT)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.28.0_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.28.0_D"
+#define DEVICE_FIRMWARE_VERSION             "3.28.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.28.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1637,7 +1637,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v0
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
 #define UVC_CLR2IR_THR4ISP                  (-50)
