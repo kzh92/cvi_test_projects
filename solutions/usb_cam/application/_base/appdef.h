b15a3da78bbbe404e1ac1cdae00dfddebf8106dc
kzh <kevin@outlook.com>
2024-01-11 17:07:41 +0800

    - 3.4.2_D  절강가사방  호위화상대화 + 람항조종기판  혼합등록방식을 막았음.  음량크기를 3.3.4_D판본에 맞추었음.  적외선화상대화현시에서 잔상을 없앴음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -469,7 +469,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_OKEDA2_UAC                    335
 #define FRM_DBS3M_TONGXIN_UVC                   336
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XINNENG_H264
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_JIASHIBANG_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -611,8 +611,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_JIASHIBANG_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.4.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.4.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.4.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.4.2_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -641,8 +641,6 @@ enum E_Baud_Rate
 #define N_MAX_HAND_NUM                      100
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
-#undef ENROLL_FACE_HAND_MODE
-#define ENROLL_FACE_HAND_MODE               ENROLL_FACE_HAND_MIX
 #undef DEFAULT_ISP_BIN_VER
 #define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
 #undef UAC_AUDALGO_USE 
@@ -653,6 +651,10 @@ enum E_Baud_Rate
 #define SPECIFIC_LOG_PRINT                  1
 #define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
 #define UVC_CLR2IR_THR4ENGINE               (-30)
+#undef USE_ISP_IR_3DNR
+#define USE_ISP_IR_3DNR                     0
+#undef UAC_SPEAKER_VOL
+#define UAC_SPEAKER_VOL                     6 // 0 ~ 32
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_KEXIONG_UAC)
