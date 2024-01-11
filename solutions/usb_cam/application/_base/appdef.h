6e66e43642d6377faf7da6ba283d4ed802137df9
kzh <kevin@outlook.com>
2024-01-11 11:44:15 +0800

    - 3.28.3_D  절강구방  칠심+람성화상대화  3.28.2_D판본에서 시작하여 색카메라현시효과를 수정하였음(v2.1.0.9).

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -469,7 +469,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_OKEDA2_UAC                    335
 #define FRM_DBS3M_TONGXIN_UVC                   336
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XINNENG_H264
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_IR_DEFAULT
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1612,8 +1612,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_IR_DEFAULT)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.28.2.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.28.2.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.28.3_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.28.3_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
