cf2b6e49fe6f2f844dde7b5263605df6bb8da948
kzh <kevin@outlook.com>
2024-01-11 22:44:11 +0800

    - 3.25.0_D  중산세기  오케다화상대화결합  3.25.0.7_D판본을 제품펌웨어로 묶었음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -469,7 +469,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_OKEDA2_UAC                    335
 #define FRM_DBS3M_TONGXIN_UVC                   336
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_JIASHIBANG_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_OKEDA2_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1512,8 +1512,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_OKEDA2_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.25.0.7_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.25.0.7_D"
+#define DEVICE_FIRMWARE_VERSION             "3.25.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.25.0_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1558,6 +1558,7 @@ enum E_Baud_Rate
 #define UVC_CLR2IR_THR4ISP                  (-50)
 #undef USE_ISP_IR_3DNR
 #define USE_ISP_IR_3DNR                     0
+
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_TONGXIN_PROTO)
 
