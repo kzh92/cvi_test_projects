2a1a37dcb9d5a5cd5a450c67cf575056731c45f4
kzh <kevin@outlook.com>
2024-01-16 17:15:58 +0800

    - 3.29.0_D  중산무건대상  화상대화설정을 3.49.8_D판본에 맞추고 여러 조종기판련동하게 하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -468,8 +468,9 @@ enum E_Baud_Rate
 #define FRM_DBS3M_TX2_UAC                       334
 #define FRM_DBS3M_OKEDA2_UAC                    335
 #define FRM_DBS3M_TONGXIN_UVC                   336
+#define FRM_DBS3M_AIPAI2_UAC                    337
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_OKEDA2_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AIPAI2_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1654,6 +1655,49 @@ enum E_Baud_Rate
 #define USE_ISP_IR_3DNR                     0
 #undef UAC_SPEAKER_VOL
 #define UAC_SPEAKER_VOL                     6 // 0 ~ 32
+
+//----------------------------------------------------------
+#elif (FRM_PRODUCT_TYPE == FRM_DBS3M_AIPAI2_UAC)
+
+#define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
+#define DEVICE_FIRMWARE_VERSION             "3.29.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.29.0_D"
+
+#undef DEFAULT_CHIP_TYPE
+#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
+#undef DEFAULT_PROTO_ENC_MODE
+#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
+#undef UVC_RES_DEFINE
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0},
+#undef ENGINE_USE_TWO_CAM
+#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
+#undef USE_3M_MODE
+#define USE_3M_MODE                         1
+#undef USE_UAC_MODE
+#define USE_UAC_MODE                        1
+#undef DEFAULT_BOARD_TYPE
+#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
+#undef DEFAULT_CAM_MIPI_TYPE
+#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
+#undef USE_VDBTASK
+#define USE_VDBTASK                         1
+#undef USE_SANJIANG3_MODE
+#define USE_SANJIANG3_MODE                  1
+#undef N_MAX_HAND_NUM
+#define N_MAX_HAND_NUM                      100
+#undef USE_WHITE_LED
+#define USE_WHITE_LED                       1
+#undef DEFAULT_ISP_BIN_VER
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
+#undef SPECIFIC_LOG_PRINT
+#define SPECIFIC_LOG_PRINT                  1
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#undef UAC_SPEAKER_VOL
+#define UAC_SPEAKER_VOL                     4 // 0 ~ 32
+#undef UAC_AUDALGO_USE
+#define UAC_AUDALGO_USE                     1
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_JINJIAN_UAC)
