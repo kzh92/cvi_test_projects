6a0b9f52789faef1134e211ab157fcf1dd869971
kzh <kevin9046@outlook.com>
2024-02-26 21:49:59 +0800

    - 3.38.1_D  심전기하  동흔+화상대화  색카메라현시효과를 수정하였음.(v2.1.0.11)  B오유가 나오기때문에 수정하였음, UAC_AUDALGO_USE=0으로 설정하였음.  원인은 찾지 못하고 B오유가 없어지는 방향으로 기능설정하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -480,7 +480,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_LAIJI_UAC                     339
 #define FRM_JIZHI_UAC                           340 //동관극지
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_DEFAULT
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_TX2_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1894,8 +1894,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_TX2_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.38.0_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.38.0_D"
+#define DEVICE_FIRMWARE_VERSION             "3.38.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.38.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1925,7 +1925,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v11
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
 #define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
@@ -1935,7 +1935,7 @@ enum E_Baud_Rate
 #undef UAC_SPEAKER_VOL
 #define UAC_SPEAKER_VOL                     4 // 0 ~ 32
 #undef UAC_AUDALGO_USE
-#define UAC_AUDALGO_USE                     1
+#define UAC_AUDALGO_USE                     0
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_BOLATAIN_MODE)
