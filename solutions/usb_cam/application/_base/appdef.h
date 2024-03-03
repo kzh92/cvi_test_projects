a42cb76fbc8d74e3f37fcdf7d8c28bb7356fcd87
kzh <kevin9046@outlook.com>
2024-03-03 16:49:26 +0800

    - 3.28.4_D 절강금간대상 람항+람성 화상대화출력해상도가 R오유에 영향을 주지 않으므로 다시 복귀하였음.안정성시험에서 R오유가  화상대화출력해상도를 수정하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -1651,7 +1651,9 @@ enum E_Baud_Rate
 #define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
-                                            {2, 640, 480, 30, 0, 6144},
+                                            {2, 864, 480, 30, 0, 10240}, \
+                                            {3, 800, 480, 30, 0, 10240}, \
+                                            {4, 640, 480, 30, 0, 6144},
 #undef ENGINE_USE_TWO_CAM
 #define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
 #undef USE_3M_MODE
