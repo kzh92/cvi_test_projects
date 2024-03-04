70a75fad249e89c2bdfd72b65c52cce740a56645
kzh <kevin9046@outlook.com>
2024-03-04 10:05:38 +0800

    - 3.1.3_D 절강일화 람항+람성7916, 손바닥기능을 지원하며 백색레드를 쓰지 않습니다. 화상대화현시효과를 수정하였음.(v2.1.0.11)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -482,14 +482,14 @@ enum E_Baud_Rate
 #define FRM_DBS3M_LAIJI_UAC                     339
 #define FRM_JIZHI_UAC                           340 //동관극지
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XIONGMAI_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_YIHE_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.1.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.1.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.1.3_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.1.3_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -521,7 +521,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v11
 #define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
