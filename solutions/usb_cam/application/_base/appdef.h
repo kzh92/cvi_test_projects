387fb2d8e9660b5515bee3dc0430846d42460920
kzh <kevin@outlook.com>
2024-03-18 17:35:05 +0800

    - 3.14.13.2_D sc201cs color v2.1.0.19 야니스대상에 리용할 람성7916화상대화에서 v2.1.0.11이 얼굴이 벌겋고 색상이 이상하다고 한 문제를 대책하기 위하여 색포화도를 떨구었음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -1093,8 +1093,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_DEFAULT)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.14.13.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.14.13.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.14.13.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.14.13.2_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1124,7 +1124,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v18
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v19
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
 #define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
