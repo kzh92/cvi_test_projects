1672951a7644e569cba9c6e7d21052521ce3294e
kzh <kevin9046@outlook.com>
2024-03-26 17:44:41 +0800

    - 3.29.1_D  하문마싸로크대상  애박두카메라화상대화+범해삼강조종기판  sc201cs color isp v2.1.0.11

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -493,7 +493,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_LAIJI_UAC                     339
 #define FRM_JIZHI_UAC                           340 //동관극지
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LAIJI_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AIPAI2_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1707,8 +1707,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_AIPAI2_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.29.0_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.29.0_D"
+#define DEVICE_FIRMWARE_VERSION             "3.29.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.29.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1735,7 +1735,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v1
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v11
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
 #undef SPECIFIC_LOG_PRINT
@@ -1745,6 +1745,8 @@ enum E_Baud_Rate
 #define UAC_SPEAKER_VOL                     4 // 0 ~ 32
 #undef UAC_AUDALGO_USE
 #define UAC_AUDALGO_USE                     1
+#undef USE_TEMP_MODE
+#define USE_TEMP_MODE                       1
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_JINJIAN_UAC)
