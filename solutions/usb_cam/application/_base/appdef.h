74d4f04cc27775d067a4041d65e197d04c238858
kzh <kevin9046@outlook.com>
2024-03-26 18:41:08 +0800

    - 3.38.1.1_D  동흔+창시기유화상대화  시험펌웨어  sc201cs color isp v2.1.0.12(v2.1.0.11보다 화상밝기를 올렸음)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -493,7 +493,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_LAIJI_UAC                     339
 #define FRM_JIZHI_UAC                           340 //동관극지
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AIPAI2_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_TX2_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1932,8 +1932,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_TX2_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.38.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.38.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.38.1.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.38.1.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1961,7 +1961,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v11
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v12
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
 #define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
