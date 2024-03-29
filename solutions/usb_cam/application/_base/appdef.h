f98f276c84060bfaa844cb41657036d14828365f
kzh <kevin9046@outlook.com>
2024-03-29 08:37:02 +0800

    - 3.13.4.1_D  중산대경대상  3.38.1.2_D판본에서 시작하여 백색레드밝기를 조절하였음.  장치적으로 10%밝기에 해당함.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -493,7 +493,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_LAIJI_UAC                     339
 #define FRM_JIZHI_UAC                           340 //동관극지
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AIPAI2_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_KELINGPU_MODE
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1061,15 +1061,16 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_KELINGPU_MODE)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.13.3_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.13.3_D"
+#define DEVICE_FIRMWARE_VERSION             "3.13.4.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.13.4.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
 #undef DEFAULT_PROTO_ENC_MODE
 #define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
 #undef UVC_RES_DEFINE
-#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0},
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
+                                            {2, 640, 480, 30, 0, 6144},
 #undef ENGINE_USE_TWO_CAM
 #define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
 #undef USE_3M_MODE
@@ -1088,8 +1089,20 @@ enum E_Baud_Rate
 #define N_MAX_HAND_NUM                      100
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
+#undef DEFAULT_ISP_BIN_VER
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v13
+#undef SPECIFIC_LOG_PRINT
+#define SPECIFIC_LOG_PRINT                  1
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#define UVC_CLR2IR_THR4ENGINE               (-30)
+#undef USE_TONGXIN_PROTO
+#define USE_TONGXIN_PROTO                   1
+#undef UAC_SPEAKER_VOL
+#define UAC_SPEAKER_VOL                     4 // 0 ~ 32
+#undef UAC_AUDALGO_USE
+#define UAC_AUDALGO_USE                     1
 #undef WLED_PWM_DUTY
-#define WLED_PWM_DUTY                       30
+#define WLED_PWM_DUTY                       15
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_DEFAULT)
