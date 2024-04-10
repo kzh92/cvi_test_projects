9f99bc339dc8774b3cc51bf657d1e71494d4aa06
kzh <kevin@outlook.com>
2024-04-10 14:58:14 +0800

    - 3.46.5_D  불산화차두  칠심+중련화상대화

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -496,7 +496,7 @@ enum E_Baud_Rate
 #define FRM_JIZHI_UAC                           340 //동관극지
 #define FRM_DBS3M_TOYA_IR_UAC                   341
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_TOYA_IR_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LIWEN_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -2281,8 +2281,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_LIWEN_UAC)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.46.4.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.46.4.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.46.5_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.46.5_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -2329,8 +2329,6 @@ enum E_Baud_Rate
 #define USE_USB_EP_ERR_FIX_MODE             1
 #undef UAC_AUDALGO_USE
 #define UAC_AUDALGO_USE                     1
-#undef UAC_SPK_EP
-#define UAC_SPK_EP                          0x83
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_DUAL_CAM_AIPAI)
