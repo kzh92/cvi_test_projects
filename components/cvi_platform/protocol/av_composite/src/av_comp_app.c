8076d9121c11437d2c2df14bbff04e62e60f7689
kzh <kevin9046@outlook.com>
2024-04-10 22:06:13 +0800

    - uvc mjpeg 720p

--- components/cvi_platform/protocol/av_composite/src/av_comp_app.c
+++ components/cvi_platform/protocol/av_composite/src/av_comp_app.c
@@ -16,8 +16,8 @@
 
 #define VIDEO_IN_EP 0x81
 
-#define WIDTH  (unsigned int)(1600)
-#define HEIGHT (unsigned int)(1200)
+#define WIDTH  (unsigned int)(1280)
+#define HEIGHT (unsigned int)(720)
 
 #define CAM_FPS        (30)
 #define INTERVAL       (unsigned long)(10000000 / CAM_FPS)
@@ -59,7 +59,7 @@ static struct usbd_endpoint audio_out_ep = {
     .ep_addr = AUDIO_OUT_EP
 };
 
-
+#if 0
 static struct uvc_frame_info_st yuy2_frame_info[] = {
     {1, 800, 600, 30, 0},
     {2, 640, 360, 30, 0},
@@ -71,16 +71,13 @@ static struct uvc_frame_info_st yuy2_frame_info[] = {
     // {9, 960, 640, 15, 0},
     // {10, 320, 240, 15, 0},
 };
+#endif
 
 static struct uvc_frame_info_st mjpeg_frame_info[] = {
-    {1, 800, 600, 30, 0},
-    {2, 640, 480, 30, 0},
-    {3, 1280, 720, 30, 0},
-    {4, 1920, 1080, 30, 0},
-    {5, 480, 320, 30, 0},
-    {6, 400, 300, 30, 0},
+    {1, 1280, 720, 30, 0},
 };
 
+#if 0
 static struct uvc_frame_info_st h264_frame_info[] = {
     {1, 800, 600, 30, 0},
     {2, 1280, 720, 30, 0},
@@ -94,12 +91,15 @@ static struct uvc_frame_info_st nv21_frame_info[] = {
     {2, 1280, 720, 30, 0},
     {3, 640, 480, 30, 0},
 };
+#endif
 
 static struct uvc_format_info_st uvc_format_info[] = {
     {MJPEG_FORMAT_INDEX, UVC_FORMAT_MJPEG, 1, ARRAY_SIZE(mjpeg_frame_info), mjpeg_frame_info},
+#if 0
     {H264_FORMAT_INDEX, UVC_FORMAT_H264, 1, ARRAY_SIZE(h264_frame_info), h264_frame_info},
     {YUYV_FORMAT_INDEX, UVC_FORMAT_YUY2, 1, ARRAY_SIZE(yuy2_frame_info), yuy2_frame_info},
     {NV21_FORMAT_INDEX, UVC_FORMAT_NV21, 1, ARRAY_SIZE(nv21_frame_info), nv21_frame_info},
+#endif
 };
 
 CVI_S32 is_media_info_update(){
@@ -241,6 +241,27 @@ void uvc_media_update(){
 	stVpssChnAttr.enPixelFormat = enPixelFormat;
 	stVpssChnAttr.u32Width = uvc_frame_info.width;
 	stVpssChnAttr.u32Height = uvc_frame_info.height;
+
+#define CLR_CAM_WIDTH 1600
+#define CLR_CAM_HEIGHT 1200
+	VPSS_CROP_INFO_S pstCropInfo;
+    MEDIA_CHECK_RET(CVI_VPSS_GetChnCrop(UVC_VPSS_GRP, UVC_VPSS_CHN, &pstCropInfo), "CVI_VPSS_GetChnCrop failed\n");
+    if (stVpssChnAttr.u32Width * 3 / 4 == stVpssChnAttr.u32Height)
+    {
+    	pstCropInfo.bEnable = CVI_FALSE;
+    }
+    else
+    {
+    	int real_width = CLR_CAM_WIDTH;
+    	int real_height = CLR_CAM_WIDTH * stVpssChnAttr.u32Height / stVpssChnAttr.u32Width;
+		pstCropInfo.bEnable = CVI_TRUE;
+		pstCropInfo.stCropRect.s32X = CLR_CAM_WIDTH > real_width ? ((CLR_CAM_WIDTH - real_width) / 2) : 0;
+		pstCropInfo.stCropRect.s32Y = CLR_CAM_HEIGHT > real_height ? ((CLR_CAM_HEIGHT - real_height) / 2) : 0;
+		pstCropInfo.stCropRect.u32Width = real_width;
+		pstCropInfo.stCropRect.u32Height = real_height;
+    }
+	MEDIA_CHECK_RET(CVI_VPSS_SetChnCrop(UVC_VPSS_GRP, UVC_VPSS_CHN, &pstCropInfo), "CVI_VPSS_SetChnCrop failed\n");
+
 	CVI_VPSS_SetChnAttr(UVC_VPSS_GRP,UVC_VPSS_CHN, &stVpssChnAttr);
 
 	pstVencCfg->pstVencChnCfg[UVC_VENC_CHN].stChnParam.u16Width = uvc_frame_info.width;
@@ -248,6 +269,7 @@ void uvc_media_update(){
 	pstVencCfg->pstVencChnCfg[UVC_VENC_CHN].stChnParam.u16EnType = enType;
 	pstVencCfg->pstVencChnCfg[UVC_VENC_CHN].stRcParam.u16BitRate = (enType == PT_MJPEG)?20480:2048;
 	pstVencCfg->pstVencChnCfg[UVC_VENC_CHN].stRcParam.u16RcMode = (enType == PT_MJPEG)?VENC_RC_MODE_MJPEGCBR:VENC_RC_MODE_H264CBR;
+	printf("uvc(%dx%d),%dbr\n", uvc_frame_info.width, uvc_frame_info.height, pstVencCfg->pstVencChnCfg[UVC_VENC_CHN].stRcParam.u16BitRate);
 
 	if(MJPEG_FORMAT_INDEX == uvc_format_info.format_index || H264_FORMAT_INDEX == uvc_format_info.format_index)
 		MEDIA_VIDEO_VencInit(pstVencCfg);
@@ -444,6 +466,7 @@ static void *send_to_uvc()
 				usbd_ep_start_write(VIDEO_IN_EP, packet_buffer_uvc, MAX_PAYLOAD_SIZE);
 				while(tx_flag && g_uvc_event_flag) {
 					aos_task_yield();
+					aos_msleep(1);
 				}
 			}
         }else {
