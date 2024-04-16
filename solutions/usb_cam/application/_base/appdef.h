82e385beab6855f1f18517eebf49ee1269dde979
kzh <kevin9046@outlook.com>
2024-04-16 23:17:16 +0800

    - 3.13.6_D  중산대경대상  절강웅매화상대화모듈결합, 3.44.9_D판본에 화상대화설정을 맞추었음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -497,7 +497,7 @@ enum E_Baud_Rate
 #define FRM_JIZHI_UAC                           340 //동관극지
 #define FRM_DBS3M_TOYA_IR_UAC                   341
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_OKEDA_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_KELINGPU_MODE
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1071,16 +1071,19 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_KELINGPU_MODE)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.13.5_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.13.5_D"
+#define DEVICE_FIRMWARE_VERSION             "3.13.6_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.13.6_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
 #undef DEFAULT_PROTO_ENC_MODE
 #define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
 #undef UVC_RES_DEFINE
-#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
-                                            {2, 640, 480, 30, 0, 6144},
+#define UVC_RES_DEFINE                      {1, 1280, 720, 15, 0, 20480}, \
+                                            {2, 864, 480, 12, 0, 9000},\
+                                            {3, 800, 480, 12, 0, 9000},\
+                                            {4, 480, 320, 12, 0, 9000},\
+                                            {5, 320, 240, 12, 0, 9000}
 #undef ENGINE_USE_TWO_CAM
 #define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
 #undef USE_3M_MODE
@@ -1100,7 +1103,9 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v13
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v11
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
 #define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
@@ -1108,7 +1113,7 @@ enum E_Baud_Rate
 #undef USE_TONGXIN_PROTO
 #define USE_TONGXIN_PROTO                   1
 #undef UAC_SPEAKER_VOL
-#define UAC_SPEAKER_VOL                     4 // 0 ~ 32
+#define UAC_SPEAKER_VOL                     16 // 0 ~ 32
 #undef UAC_AUDALGO_USE
 #define UAC_AUDALGO_USE                     1
 #undef WLED_PWM_DUTY
