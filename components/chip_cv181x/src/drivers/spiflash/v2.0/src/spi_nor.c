446b2a242c9396de68181c0f6193ece2a539f50b
kzh <kevin9046@outlook.com>
2024-04-20 22:05:25 +0800

    - 갱신된 SDK20240410의 SPI NOR Flash드라이버를 반영한 boot를 만들고 리용하였음.
    # Conflicts:
    #	components/chip_cv181x/src/drivers/spiflash/v2.0/src/spi_nor.c

--- components/chip_cv181x/src/drivers/spiflash/v2.0/src/spi_nor.c
+++ components/chip_cv181x/src/drivers/spiflash/v2.0/src/spi_nor.c
@@ -57,6 +57,10 @@ const struct flash_info spi_flash_ids[] = {
 			RD_FULL | WR_FULL | SECT_4K) },
 	{ "XM25QH256C", INFO(0x204019, 0x0, 64 * 1024, 512,
 			RD_FULL | WR_FULL | SECT_4K | NOR_4B_OPCODES) },
+	{ "XM25QH256B", INFO(0x206019, 0x0, 64 * 1024, 512,
+			RD_FULL | WR_FULL | SECT_4K | NOR_4B_OPCODES) },
+	{ "XM25QU256B", INFO(0x207019, 0x0, 64 * 1024, 512,
+			RD_FULL | WR_FULL | SECT_4K | NOR_4B_OPCODES) },
 	{ "IS25LP128F", INFO(0x9d4018, 0x0, 64 * 1024, 256,
 			RD_FULL | WR_FULL | SECT_4K) },
 	{ "IS25LP256E", INFO(0x9d6019, 0x0, 64 * 1024, 512,
@@ -81,14 +85,28 @@ const struct flash_info spi_flash_ids[] = {
 		RD_FULL | WR_QPP | SECT_4K) },
 	{ "FM25Q64", INFO(0xF83217, 0x0, 64 * 1024, 128,
 		RD_FULL | WR_QPP | SECT_4K) },
+	{ "FM25Q64A", INFO(0xA14017, 0x0, 64 * 1024, 128,
+		RD_FULL | WR_QPP | SECT_4K) },
+	{ "FM25Q128A", INFO(0xA14018, 0x0, 64 * 1024, 128,
+		RD_FULL | WR_QPP | SECT_4K) },
+    { "BY25Q64ES", INFO(0x684017, 0x0, 64 * 1024, 128,
+		RD_FULL | WR_QPP | SECT_4K) },
 	{ "BY25Q128AS", INFO(0x684018, 0x0, 64 * 1024, 256,
 		RD_FULL | WR_QPP | SECT_4K) },
 	{ "BY25Q256FS", INFO(0x684919, 0x0, 64 * 1024, 512,
 		RD_FULL | WR_QPP | SECT_4K | NOR_4B_OPCODES) },
 	{ "PY25Q128HA", INFO(0x852018, 0x0, 64 * 1024, 256,
 		RD_FULL | WR_QPP | SECT_4K) },
+	{ "PY25Q64HA", INFO(0x852017, 0x0, 64 * 1024, 128,
+		RD_FULL | WR_QPP | SECT_4K) },
 	{ "P25Q64SH", INFO(0x856017, 0x0, 64 * 1024, 128,
 		RD_FULL | WR_QPP | SECT_4K) },
+	{ "PY25Q256H", INFO(0x852019, 0x0, 64 * 1024, 512,
+		RD_FULL | WR_FULL | SECT_4K | NOR_4B_OPCODES) },
+	{ "GM25Q64A", INFO(0x1c4017, 0x0, 64 * 1024, 128,
+		RD_FULL | WR_QPP | SECT_4K) },
+	{ "GD25F64F", INFO(0xc84317, 0x0, 64 * 1024, 128,
+		RD_FULL | WR_QPP | SECT_4K | _10_DUMMY_CYCLE | NO_QE | ADJUST_DUMMY) },
 
 	{},     /* Empty entry to terminate the list */
 
@@ -270,7 +288,7 @@ void match_read_op(struct spi_nor *nor, uint32_t flags)
 }
 
 void match_write_op(struct spi_nor *nor, uint32_t flags)
-{	
+{
 	struct flash_info *info = nor->info;
 	spi_tran_conf_t   *write = &nor->write_op;
 
@@ -373,6 +391,19 @@ static int read_sr2(struct spi_nor *nor, uint8_t *val)
 	return ret;
 }
 
+static int read_sr3(struct spi_nor *nor, uint8_t *val)
+{
+	int ret = 0;
+
+	ret = nor->read_reg(nor, 0x15, val, 1);
+	if (ret < 0) {
+		printf("error %d reading SR\n", (int) ret);
+		return ret;
+	}
+
+	return ret;
+}
+
 /*
  * return value : 1 means that flash is ready, 0 means that flash is busy.
  */
@@ -455,6 +486,21 @@ static int write_sr2(struct spi_nor *nor, uint8_t sr)
 	return 0;
 }
 
+static int write_sr3(struct spi_nor *nor, uint8_t sr)
+{
+	ssize_t ret;
+	write_enable(nor);
+	ret = nor->write_reg(nor, 0x11, &sr, 1);
+	if (ret < 0) {
+		return -1;
+	}
+	ret = spi_nor_ready(nor);
+	if (ret) {
+		return ret;
+	}
+	return 0;
+}
+
 static int write_sr(struct spi_nor *nor, uint8_t sr)
 {
 	ssize_t ret;
@@ -469,6 +515,34 @@ static int write_sr(struct spi_nor *nor, uint8_t sr)
 	if (ret) {
 		return ret;
 	}
+	return 0;
+}
+
+int set_dummy(struct spi_nor *nor)
+{
+	int ret;
+	uint8_t val = 0;
+	ret = read_sr3(nor, &val);
+	if (ret < 0)
+		return ret;
+
+	if ((val & 0x3) == 0x3) {
+		return 0;
+	}
+
+	write_enable(nor);
+	write_sr3(nor, val | 0x3);
+	ret = spi_nor_ready(nor);
+	if (ret)
+		return ret;
+
+	val = 0;
+	ret = read_sr3(nor, &val);
+	if (!(ret >= 0 && (val & 0x3))) {
+		printf("SPINOR: SR3[0-1] dummy clk bit was set failed!\n");
+		return -1;
+	}
+
 	return 0;
 }
 
@@ -550,6 +624,9 @@ static int set_quad_mode(struct spi_nor *nor)
 		case SNOR_MFR_JUYANG:
 		case SNOR_MFR_ZBIT:
 		case SNOR_MFR_XMC:
+			if (info->id[1] == 0x60) {
+				return quad_enable_SR_bit6(nor);
+			}
 		case SNOR_MFR_XTX:
 		case SNOR_MFR_FM:
 		case SNOR_MFR_SPANSION:
@@ -630,9 +707,10 @@ int adjust_erase_argumen(struct spi_nor *nor, struct erase_op *op, uint32_t to, 
 	}
 	//printf("[%s]:==> %s\n", __func__, tmp_op->desc);
 
-	if (nor->addr_width == 4 && (nor->info->flags & NOR_4B_OPCODES))
+	if (nor->addr_width == 4 && (nor->info->flags & NOR_4B_OPCODES)) {
 		nor->erase_opcode = spi_nor_convert_3to4_erase(nor->erase_opcode);
-
+		op->opcode = spi_nor_convert_3to4_erase(op->opcode);
+	}
 	return 0;
 }
 
@@ -666,7 +744,7 @@ int cvi_spif_erase(struct spi_nor *nor, uint32_t to, uint32_t len)
 			if (ret)
 				return ret;
 
-			erasesize = nor->erase_size;
+			erasesize = op.size;
 			to += erasesize;
 			len -= erasesize;
 
@@ -765,8 +843,11 @@ int spi_nor_rescan(struct spi_nor *nor)
 	match_read_op(nor, info->flags);
 	match_write_op(nor, info->flags);
 
-	if (info->flags & (RD_QUADIO | WR_QUAD | RD_QUAD))
+	if (!(info->flags & NO_QE) && (info->flags & (RD_QUADIO | WR_QUAD | RD_QUAD)))
 		set_quad_mode(nor);
+
+	if (info->flags & ADJUST_DUMMY)
+		set_dummy(nor);
 
 #ifndef USE_4K_ERASE_SECTION
 	nor->erase_opcode = CVI_SPINOR_OP_SE;
