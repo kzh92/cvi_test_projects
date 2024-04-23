3eda3d6765c43e66ab8cec56fd2e0ce69213824a
kzh <kevin@outlook.com>
2024-04-23 20:08:38 +0800

    - disable usb logs

--- boards/cv180xb_evb/include/usb_config.h
+++ boards/cv180xb_evb/include/usb_config.h
@@ -15,7 +15,7 @@
 #define usb_free(ptr)    free(ptr)
 
 #ifndef CONFIG_USB_DBG_LEVEL
-#define CONFIG_USB_DBG_LEVEL USB_DBG_INFO
+#define CONFIG_USB_DBG_LEVEL USB_DBG_ERROR
 #endif
 
 /* Enable print with color */
