e31fbcec36c381986a1d3b45afb5966cae641790
kzh <kevin9046@outlook.com>
2024-04-26 14:59:30 +0800

    - 3.60.0.1_D  3.14.14_D와 기능이 같음  장치검사용펌웨어

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -499,7 +499,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_SAINAO                        342
 #define FRM_DBS3M_SAINAO_TUYA                   343
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XINNENG_H264
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_DEFAULT
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1120,9 +1120,10 @@ enum E_Baud_Rate
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_DEFAULT)
 
+// 3.14.14_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.14.14_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.14.14_D"
+#define DEVICE_FIRMWARE_VERSION             "3.60.0.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.60.0.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
