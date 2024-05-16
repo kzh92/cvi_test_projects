99260f3b4bb02f9fce0621ac3dcd127d06fb6da9
kzh <kevin9046@outlook.com>
2024-05-17 00:47:01 +0800

    - 3.65.0_D  절강흥려대상  3.24.3_D과 설정을 같게 하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -501,7 +501,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_SAINAO_TUYA                   343
 #define FRM_DBS3M_LIWEN_IR                      344
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LIWEN_IR
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_HONGLI_MODE
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1523,35 +1523,20 @@ enum E_Baud_Rate
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_HONGLI_MODE)
 
+//3.24.3_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.24.3_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.24.3_D"
+#define DEVICE_FIRMWARE_VERSION             "3.65.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.65.0_D"
 
-#undef DEFAULT_CHIP_TYPE
-#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
-#undef DEFAULT_PROTO_ENC_MODE
-#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0, 10240}, \
                                             {2, 640, 480, 30, 0, 8192}, \
                                             {3, 480, 320, 30, 0, 4096}, \
                                             {4, 320, 240, 30, 0, 2048},
-#undef ENGINE_USE_TWO_CAM
-#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
 #undef USE_3M_MODE
 #define USE_3M_MODE                         1
 #undef USE_UAC_MODE
 #define USE_UAC_MODE                        0
-#undef DEFAULT_BOARD_TYPE
-#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
-#undef DEFAULT_CAM_MIPI_TYPE
-#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
-#undef USE_VDBTASK
-#define USE_VDBTASK                         1
-#undef USE_SANJIANG3_MODE
-#define USE_SANJIANG3_MODE                  1
-#undef N_MAX_HAND_NUM
-#define N_MAX_HAND_NUM                      100
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
 #undef DEFAULT_ISP_BIN_VER
