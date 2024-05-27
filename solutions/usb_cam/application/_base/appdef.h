8fb56aed92cf2829fa636463214579048b871545
darkhorse <darkhorse215@outlook.com>
2024-05-27 22:14:57 +0800

    - 3.72.0_D  하문마싸로크대상  애박두카메라화상대화+범해삼강조종기판

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -517,7 +517,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_KEYU_UAC                      346
 #define FRM_DBS3MH_DEFAULT                      347
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_HONGLI_MODE
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AIPAI2_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1715,32 +1715,15 @@ enum E_Baud_Rate
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_AIPAI2_UAC)
 
+//3.29.1_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.29.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.29.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.72.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.72.0_D"
 
-#undef DEFAULT_CHIP_TYPE
-#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
-#undef DEFAULT_PROTO_ENC_MODE
-#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0},
-#undef ENGINE_USE_TWO_CAM
-#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
 #undef USE_3M_MODE
 #define USE_3M_MODE                         1
-#undef USE_UAC_MODE
-#define USE_UAC_MODE                        1
-#undef DEFAULT_BOARD_TYPE
-#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
-#undef DEFAULT_CAM_MIPI_TYPE
-#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
-#undef USE_VDBTASK
-#define USE_VDBTASK                         1
-#undef USE_SANJIANG3_MODE
-#define USE_SANJIANG3_MODE                  1
-#undef N_MAX_HAND_NUM
-#define N_MAX_HAND_NUM                      100
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
 #undef DEFAULT_ISP_BIN_VER
@@ -1753,10 +1736,6 @@ enum E_Baud_Rate
 #define UVC_CLR2IR_THR4ENGINE               (-30)
 #undef UAC_SPEAKER_VOL
 #define UAC_SPEAKER_VOL                     4 // 0 ~ 32
-#undef UAC_AUDALGO_USE
-#define UAC_AUDALGO_USE                     1
-#undef USE_TEMP_MODE
-#define USE_TEMP_MODE                       1
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_JINJIAN_UAC)
