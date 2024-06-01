823d7f523901b90e126fbcd47c70df6d25707507
kzh <kevin9046@outlook.com>
2024-06-01 16:38:53 +0800

    - 3.60.1.1_D  중산순휘대상  색카메라현시효과 시험펌웨어  sc201cs color isp v2.1.0.23

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -1089,8 +1089,8 @@ enum E_Baud_Rate
 
 // 3.14.14_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.60.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.60.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.60.1.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.60.1.1_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1120,7 +1120,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       1
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v11
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v23
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
 #define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
