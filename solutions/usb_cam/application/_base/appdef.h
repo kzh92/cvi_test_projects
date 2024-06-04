d235e010a9f4c11191b28eb05a0b9e36e3f6536d
darkhorse <darkhorse215@outlook.com>
2024-06-04 23:02:48 +0800

    - 3.65.2_D  절강흥려대상

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -521,7 +521,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_FANHAI_IR_MODE                349
 #define FRM_DBS3M_LS7258_UAC                    350
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_KEXIONG_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_HONGLI_MODE
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1509,8 +1509,8 @@ enum E_Baud_Rate
 
 //3.24.3_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.65.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.65.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.65.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.65.2_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0, 10240}, \
