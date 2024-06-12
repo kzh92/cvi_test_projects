b406bf9317a55dad653b02e185939900e2505bd9
darkhorse <darkhorse215@outlook.com>
2024-06-12 15:22:47 +0800

    - 3.84.1_D  심전화억 FRM3M5.0DBSS(3.01AC) 1차생산(해외판매용)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -2265,34 +2265,16 @@ enum E_Baud_Rate
 
 //3.43.6_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.84.0.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.84.0.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.84.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.84.1_D"
 
-#undef DEFAULT_CHIP_TYPE
-#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
-#undef DEFAULT_PROTO_ENC_MODE
-#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
                                             {2, 864, 480, 30, 0, 10240}, \
                                             {3, 800, 480, 30, 0, 10240}, \
                                             {4, 640, 480, 30, 0, 6144},
-#undef ENGINE_USE_TWO_CAM
-#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
 #undef USE_3M_MODE
 #define USE_3M_MODE                         U3M_SEMI
-#undef USE_UAC_MODE
-#define USE_UAC_MODE                        1
-#undef DEFAULT_BOARD_TYPE
-#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
-#undef DEFAULT_CAM_MIPI_TYPE
-#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
-#undef USE_VDBTASK
-#define USE_VDBTASK                         1
-#undef USE_SANJIANG3_MODE
-#define USE_SANJIANG3_MODE                  1
-#undef N_MAX_HAND_NUM
-#define N_MAX_HAND_NUM                      100
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef ENROLL_FACE_HAND_MODE
@@ -2304,6 +2286,8 @@ enum E_Baud_Rate
 #define UVC_CLR2IR_THR4ISP                  (-50)
 #undef USE_TEMP_MODE
 #define USE_TEMP_MODE                       1
+#undef UVC_DARK_WATCH_COUNTER
+#define UVC_DARK_WATCH_COUNTER              10
 #define ENGINE_FOR_ABROAD
 
 //----------------------------------------------------------
