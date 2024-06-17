774536e965288c822c68f5bb0847d233901262c4
kzh <kevin9046@outlook.com>
2024-06-17 14:35:00 +0800

    - 3.64.2_D 중산리문대상 UAC음량을 4으로 낮추었음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -533,7 +533,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_SH_UAC                        356
 #define FRM_DBS3M_FUSHI_XM_UAC                  357
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LS7258_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LIWEN_IR
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1949,8 +1949,8 @@ enum E_Baud_Rate
 
 // 3.35.0_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.64.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.64.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.64.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.64.2_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1969,7 +1969,7 @@ enum E_Baud_Rate
 #define SPECIFIC_LOG_PRINT                  1
 #define UVC_CLR2IR_THR4ISP                  (-50)
 #undef UAC_SPEAKER_VOL
-#define UAC_SPEAKER_VOL                     6 // 0 ~ 32
+#define UAC_SPEAKER_VOL                     4 // 0 ~ 32
 #undef UVC_DARK_WATCH_COUNTER
 #define UVC_DARK_WATCH_COUNTER              10
 
