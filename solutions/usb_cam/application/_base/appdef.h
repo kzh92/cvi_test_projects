a7565892b90d2600575479de3b63677f1bf79146
kzh <kevin9046@outlook.com>
2024-06-22 09:22:07 +0800

    - 3.90.0_D  절강균리(3.32.6_D)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -540,7 +540,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_LS7258_IR_UAC                 358
 #define FRM_DBS3M_YIHE2_UAC                     359
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LIWEN_IR
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LS35_LH_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1859,35 +1859,18 @@ enum E_Baud_Rate
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_LS35_LH_UAC)
 
+// 3.32.6_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.32.6_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.32.6_D"
+#define DEVICE_FIRMWARE_VERSION             "3.90.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.90.0_D"
 
-#undef DEFAULT_CHIP_TYPE
-#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
-#undef DEFAULT_PROTO_ENC_MODE
-#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0, 10240}, \
                                             {2, 640, 480, 30, 0, 4096},
 #undef DEFAULT_UVC_DIR
 #define DEFAULT_UVC_DIR                     UVC_ROTATION_270
-#undef ENGINE_USE_TWO_CAM
-#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
 #undef USE_3M_MODE
 #define USE_3M_MODE                         U3M_SEMI
-#undef USE_UAC_MODE
-#define USE_UAC_MODE                        1
-#undef DEFAULT_BOARD_TYPE
-#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
-#undef DEFAULT_CAM_MIPI_TYPE
-#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
-#undef USE_VDBTASK
-#define USE_VDBTASK                         1
-#undef USE_SANJIANG3_MODE
-#define USE_SANJIANG3_MODE                  1
-#undef N_MAX_HAND_NUM
-#define N_MAX_HAND_NUM                      100
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
@@ -1895,10 +1878,6 @@ enum E_Baud_Rate
 #define UVC_CLR2IR_THR4ISP                  (-200)
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
-#undef USE_ISP_IR_3DNR
-#define USE_ISP_IR_3DNR                     0
-#undef USE_TEMP_MODE
-#define USE_TEMP_MODE                       1
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_TX_PROTO_IR)
