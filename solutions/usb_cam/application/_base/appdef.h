15eb24fc4035e980a6aba1b8352696d7481a7f9c
kzh <kevin9046@outlook.com>
2024-06-26 15:26:22 +0800

    - 3.92.0_D  불산화차두(3.46.6_D)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -541,7 +541,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_YIHE2_UAC                     359
 #define FRM_DBS3M_IR_JIGAO                      360
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_IR_JIGAO
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LIWEN_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -2348,36 +2348,17 @@ enum E_Baud_Rate
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_LIWEN_UAC)
 
+// 3.46.6_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.46.5_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.46.5_D"
+#define DEVICE_FIRMWARE_VERSION             "3.92.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.92.0_D"
 
-#undef DEFAULT_CHIP_TYPE
-#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
-#undef DEFAULT_PROTO_ENC_MODE
-#define DEFAULT_PROTO_ENC_MODE              PROTO_EM_ENCRYPT_XOR_LANHENG
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0, 10240},
-#undef ENGINE_USE_TWO_CAM
-#define ENGINE_USE_TWO_CAM                  EUTC_3M_MODE
 #undef USE_3M_MODE
 #define USE_3M_MODE                         U3M_SEMI
-#undef USE_UAC_MODE
-#define USE_UAC_MODE                        1
-#undef UAC_SAMPLE_RATE
-#define UAC_SAMPLE_RATE                     16000
 #undef UAC_SPEAKER_VOL
 #define UAC_SPEAKER_VOL                     8
-#undef DEFAULT_BOARD_TYPE
-#define DEFAULT_BOARD_TYPE                  BD_TY_FMDBSS_1V0J
-#undef DEFAULT_CAM_MIPI_TYPE
-#define DEFAULT_CAM_MIPI_TYPE               CAM_MIPI_TY_122
-#undef USE_VDBTASK
-#define USE_VDBTASK                         1
-#undef USE_SANJIANG3_MODE
-#define USE_SANJIANG3_MODE                  1
-#undef N_MAX_HAND_NUM
-#define N_MAX_HAND_NUM                      100
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
@@ -2389,14 +2370,14 @@ enum E_Baud_Rate
 #define UVC_INIT_WIDTH                      1280
 #undef UVC_INIT_HEIGHT
 #define UVC_INIT_HEIGHT                     720
-#undef USE_WATCHDOG
-#define USE_WATCHDOG                        1
 #undef UVC_USBD_PRINT
 #define UVC_USBD_PRINT                      1
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
-#undef UAC_AUDALGO_USE
-#define UAC_AUDALGO_USE                     1
+#undef ENROLL_ANGLE_MODE
+#define ENROLL_ANGLE_MODE                   1
+#undef UVC_DARK_WATCH_COUNTER
+#define UVC_DARK_WATCH_COUNTER              10
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_XINAN)
