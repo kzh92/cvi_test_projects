ca8b8d0a90025aa79b70bd16e8da82f991578c52
kzh <kevin9046@outlook.com>
2024-06-27 20:07:25 +0800

    - 3.60.1.5_D  일반대상  CONFIG_SPI_NOR_ER_TIME=20으로 수정한 시험펌웨어

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -543,7 +543,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_IR_JIGAO                      360
 #define FRM_DBS3M_BK7258_UAC                    361
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_KEXIONG_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_DEFAULT
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1084,8 +1084,8 @@ enum E_Baud_Rate
 
 // 3.14.14_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.60.1.4_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.60.1.4_D"
+#define DEVICE_FIRMWARE_VERSION             "3.60.1.5_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.60.1.5_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -1127,7 +1127,7 @@ enum E_Baud_Rate
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
 #undef CONFIG_SPI_NOR_ER_TIME
-#define CONFIG_SPI_NOR_ER_TIME              2000
+#define CONFIG_SPI_NOR_ER_TIME              20
 
 //----------------------------------------------------------
 
