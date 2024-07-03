12e1ba41af861d3fcfcecbc7fa60dc19458cfe7c
kzh <kevin9046@outlook.com>
2024-07-03 16:46:18 +0800

    - 3.73.1_D  웅매7258화상대화련동  정시회사에서 수정한 uvc_uac_fail1.patch(2024/6/27)코드를 결합한데 맞게 마크로를 수정하였음.  웅매7258화상대화모듈과 련동할때 무음상태에서 얼굴인식모듈의 고성기로 잡음이 출력되던 문제를 대책하였음.(잡음을 수동제거하도록 하였음.)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -545,8 +545,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_BK7258_UAC                    361
 #define FRM_DBS3M_XINGUOXIN_XIONGMAI_UAC        362
 
-
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_KEXIONG_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XM7258_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -2709,10 +2708,11 @@ enum E_Baud_Rate
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_XM7258_UAC)
+
 // 3.55.0
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.73.0_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.73.0_D"
+#define DEVICE_FIRMWARE_VERSION             "3.73.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.73.1_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0, 0},\
@@ -2728,10 +2728,10 @@ enum E_Baud_Rate
 #define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v11
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
-#undef SPECIFIC_LOG_PRINT
-#define SPECIFIC_LOG_PRINT                  1
-#undef UVC_USBD_PRINT
-#define UVC_USBD_PRINT                      1
+// #undef SPECIFIC_LOG_PRINT
+// #define SPECIFIC_LOG_PRINT                  1
+// #undef UVC_USBD_PRINT
+// #define UVC_USBD_PRINT                      1
 #define UVC_CLR2IR_THR4ISP                  (-50)
 #undef UVC_DARK_WATCH_COUNTER
 #define UVC_DARK_WATCH_COUNTER              10
@@ -2739,6 +2739,8 @@ enum E_Baud_Rate
 #define CONFIG_USB_HS                       0
 #undef CONFIG_DWC2_VERSION
 #define CONFIG_DWC2_VERSION                 1
+#undef UAC_SPK_NR_USE
+#define UAC_SPK_NR_USE                      1
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE2_UAC)
