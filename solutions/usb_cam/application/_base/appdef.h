583e7fb196bfb79d958eac652f09eb188a75bf4a
kzh <kevin9046@outlook.com>
2024-07-08 17:36:35 +0800

    - 3.98.0_D  하문마싸로크대상  애박세카메라7258화상대화결합  짝수판본: 백색레드 쓰지 않는 방안  홀수판본: 백색레드를 쓰는 방안  3.73.1_D판본에서 시작하여 색카메라현시효과를 v2.1.0.13으로 수정하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -545,8 +545,9 @@ enum E_Baud_Rate
 #define FRM_DBS3M_BK7258_UAC                    361
 #define FRM_DBS3M_XINGUOXIN_XIONGMAI_UAC        362
 #define FRM_DBS3M_BK7256_UAC                    363
+#define FRM_DBS3M_AP7258_UAC                    364
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_FUSHI_XM_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AP7258_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -3155,6 +3156,50 @@ odd version: use_whiteled = 1
 #define ENROLL_FACE_HAND_MODE               ENROLL_FACE_HAND_MIX
 #undef DEFAULT_UVC_PIXEL_FMT
 #define DEFAULT_UVC_PIXEL_FMT               UVC_PIXEL_FMT_YUV422
+
+//----------------------------------------------------------
+#elif (FRM_PRODUCT_TYPE == FRM_DBS3M_AP7258_UAC)
+
+// 3.73.1
+#define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
+#define DEVICE_FIRMWARE_VERSION             "3.98.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.98.0_D"
+
+#undef UVC_RES_DEFINE
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0, 0},\
+                                            {2, 864, 480, 30, 0, 10240},\
+                                            {3, 800, 480, 30, 0, 9000},
+#undef UAC_SPEAKER_VOL
+#define UAC_SPEAKER_VOL                     16
+#undef USE_WHITE_LED
+#define USE_WHITE_LED                       0
+#undef DEFAULT_ISP_BIN_VER
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v13
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
+#undef CONFIG_USB_HS
+#define CONFIG_USB_HS                       0
+#undef CONFIG_DWC2_VERSION
+#define CONFIG_DWC2_VERSION                 1
+#undef UAC_SPK_NR_USE
+#define UAC_SPK_NR_USE                      1
+
+#if (USE_WHITE_LED == 0)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_SEMI
+#define UVC_CLR2IR_THR4ISP                  (-50)
+#undef UVC_DARK_WATCH_COUNTER
+#define UVC_DARK_WATCH_COUNTER              10
+
+#elif (USE_WHITE_LED == 1)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_DEFAULT
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#define UVC_CLR2IR_THR4ENGINE               (-30)
+
+#else // USE_WHITE_LED
+#error "USE_WHITE_LED must be 0 or 1."
+#endif // USE_WHITE_LED
 
 //----------------------------------------------------------
 #endif // FRM_PRODUCT_TYPE
