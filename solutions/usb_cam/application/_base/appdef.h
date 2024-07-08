29a4dcf5f3be69e80a261649ad61693d2b687052
kzh <kevin9046@outlook.com>
2024-07-08 21:33:16 +0800

    - 3.87.6_D  3.63.1_D판본에서 시작하여  절강소수대상에 맞게 수정한 색카메라현시효과 v2.1.0.38을 결합하였음.  적외선카메라인식방향을 180도 회전하였음.  화상대화현시방향을 180도 회전하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -547,7 +547,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_BK7256_UAC                    363
 #define FRM_DBS3M_AP7258_UAC                    364
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LIWEN_IR
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_JINJIAN_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1755,8 +1755,8 @@ enum E_Baud_Rate
 
 // 3.30.1_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.87.4_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.87.4_D"
+#define DEVICE_FIRMWARE_VERSION             "3.87.6_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.87.6_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
