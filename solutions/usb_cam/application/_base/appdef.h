c6e568350ef94557bc23c295a7693e061a12a045
kzh <kevin9046@outlook.com>
2024-07-12 22:44:48 +0800

    - 3.62.2.3_D  광주적고대상에서 화상대화할때 적외선방식으로 절환하면 화상용량이 커서 화상대화모듈이 폭주되는 문제가 있다고 하므로 UVC로 출력하는 화상용량을 줄인 시험펌웨어

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -553,18 +553,18 @@ enum E_Baud_Rate
 #define FRM_DBS3M_AP7258_UAC                    364
 #define FRM_DBS3M_JIARUI_UAC                    365
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_TX_PROTO_IR
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_YIHE_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
 
 // 3.1.5_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.62.2.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.62.2.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.62.2.3_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.62.2.3_D"
 
 #undef UVC_RES_DEFINE
-#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0, 10240}, \
                                             {2, 864, 480, 30, 0, 10240}, \
                                             {3, 800, 480, 30, 0, 10240}, \
                                             {4, 640, 480, 30, 0, 6144},
@@ -573,7 +573,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v46
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v11
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
 #define UVC_CLR2IR_THR4ISP                  (-50)
