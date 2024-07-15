f22d8a769e3b1ca530b04b1433298c8fb3c07615
kzh <kevin9046@outlook.com>
2024-07-15 21:04:59 +0800

    - 3.74.2_D 범해삼강조종기판+범해삼강화상대화(3.18.6_D) 짝수판본: 백색레드를 쓰지 않음. 홀수판본: 백색레드를 리용함. 3.18.6_D에 맞추어 aos_debug_printf()가 동작하게 하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -554,7 +554,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_AP7258_UAC                    364
 #define FRM_DBS3M_JIARUI_UAC                    365
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LS7258_IR_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_FANHAI_IR_MODE
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1281,15 +1281,13 @@ enum E_Baud_Rate
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_FANHAI_IR_MODE)
 
-//3.71.0_D
+// 3.18.6_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.74.0_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.74.0_D"
+#define DEVICE_FIRMWARE_VERSION             "3.74.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.74.2_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0},
-#undef USE_3M_MODE
-#define USE_3M_MODE                         U3M_SEMI
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef ENROLL_FACE_HAND_MODE
@@ -1300,9 +1298,23 @@ enum E_Baud_Rate
 #define UAC_SPK_EP                          0x83
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
-#define UVC_CLR2IR_THR4ISP                  (-50) //threshold value for turning white led on.
+
+#if (USE_WHITE_LED == 0)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_SEMI
+#define UVC_CLR2IR_THR4ISP                  (-50)
 #undef UVC_DARK_WATCH_COUNTER
 #define UVC_DARK_WATCH_COUNTER              10
+
+#elif (USE_WHITE_LED == 1)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_DEFAULT
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#define UVC_CLR2IR_THR4ENGINE               (-30)
+
+#else // USE_WHITE_LED
+#error "USE_WHITE_LED must be 0 or 1."
+#endif // USE_WHITE_LED
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_JIGAO_UAC)
