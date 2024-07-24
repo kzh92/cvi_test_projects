152ea17b3db0477fc639246dbe0bdede817613d3
kzh <kevin9046@outlook.com>
2024-07-24 22:01:26 +0800

    - 3.71.2.1_D 3.62.1_D판본에서 시작.CONFIG_DWC2_VERSION=0 손바닥인식기능을 막았음. 940nm를 쓰는데 맞게 카메라조종론리를 수정한 엔진을 결합하였음. sc201cs color isp v2.1.0.11 짝수판본: 백색레드 쓰지 않는 방안 홀수판본: 백색레드 쓰는 방안

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -572,7 +572,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_JIARUI_UAC                    365
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_KEXIONG_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_FANHAI_MODE
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1270,8 +1270,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_FANHAI_MODE)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.71.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.71.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.71.2.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.71.2.1_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
@@ -1300,7 +1300,7 @@ enum E_Baud_Rate
 #undef UVC_DARK_WATCH_COUNTER
 #define UVC_DARK_WATCH_COUNTER              10
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_211v0
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v11
 
 #elif (USE_WHITE_LED == 1)
 #undef USE_3M_MODE
