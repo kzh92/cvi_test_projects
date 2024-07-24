2726545c45342bf91c06fda1fc2ace4613a5be54
kzh <kevin9046@outlook.com>
2024-07-24 16:08:22 +0800

    - 3.75.4.2_D  람성7258화상대화련동  음량 32,yuv422  sc201cs color isp v2.1.0.49  짝수판본: 백색레드를 쓰지 않는 방안  홀수판본: 백색레드를 쓰는 방안

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -572,7 +572,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_JIARUI_UAC                    365
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XM7258_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LS7258_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -2209,8 +2209,8 @@ enum E_Baud_Rate
 
 // 3.41.0
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.75.4.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.75.4.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.75.4.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.75.4.2_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
@@ -2220,7 +2220,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v48
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v49
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
 // #undef SPECIFIC_LOG_PRINT
@@ -2232,9 +2232,9 @@ enum E_Baud_Rate
 #undef CONFIG_DWC2_VERSION
 #define CONFIG_DWC2_VERSION                 1
 #undef UAC_SPEAKER_VOL
-#define UAC_SPEAKER_VOL                     10 // 0 ~ 32
-// #undef DEFAULT_UVC_PIXEL_FMT
-// #define DEFAULT_UVC_PIXEL_FMT               UVC_PIXEL_FMT_YUV422
+#define UAC_SPEAKER_VOL                     32 // 0 ~ 32
+#undef DEFAULT_UVC_PIXEL_FMT
+#define DEFAULT_UVC_PIXEL_FMT               UVC_PIXEL_FMT_YUV422
 #undef UAC_SPK_NR_USE
 #define UAC_SPK_NR_USE                      2
 
