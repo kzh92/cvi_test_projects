65e01f9c378aeb90fb4e48680c3e7d7e4a04d4c1
kzh <kevin9046@outlook.com>
2024-07-29 20:52:54 +0800

    - 3.76.2_D  애박도아화상대화모듈  색카메라화상의 화질을 개선한 ISP조종결과물 v2.1.0.49를 결합하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -573,7 +573,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_JIARUI_UAC                    365
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XIONGMAI_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AIPAI_TOYA_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1035,31 +1035,44 @@ enum E_Baud_Rate
 
 // 3.11.4_D + 3.1.6_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.76.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.76.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.76.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.76.2_D"
 
-#undef DEFAULT_CHIP_TYPE
-#define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
 #undef UVC_RES_DEFINE
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0, 10240}, \
                                             {2, 864, 480, 30, 0, 10240}, \
                                             {3, 800, 480, 30, 0, 10240}, \
                                             {4, 640, 480, 30, 0, 6144},
-#undef USE_3M_MODE
-#define USE_3M_MODE                         1
 #undef USE_WHITE_LED
-#define USE_WHITE_LED                       1
+#define USE_WHITE_LED                       0
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v21
-#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
-#define UVC_CLR2IR_THR4ENGINE               (-30)
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v49
 #undef UAC_SPK_EP
 #define UAC_SPK_EP                          0x83
-#undef UVC_USBD_PRINT
-#define UVC_USBD_PRINT                      1
+// #undef UVC_USBD_PRINT
+// #define UVC_USBD_PRINT                      1
+#undef CONFIG_DWC2_VERSION
+#define CONFIG_DWC2_VERSION                 1
+
+#if (USE_WHITE_LED == 0)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_SEMI
+#define UVC_CLR2IR_THR4ISP                  (-50)
+#undef UVC_DARK_WATCH_COUNTER
+#define UVC_DARK_WATCH_COUNTER              10
+
+#elif (USE_WHITE_LED == 1)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_DEFAULT
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#define UVC_CLR2IR_THR4ENGINE               (-30)
+
+#else // USE_WHITE_LED
+#error "USE_WHITE_LED must be 0 or 1."
+#endif // USE_WHITE_LED
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_XINNENG_UAC)
