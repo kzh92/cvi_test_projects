593cac6e856c2072cec8ed5b52af38e3588863cf
darkhorse <darkhorse215@outlook.com>
2024-08-03 11:56:14 +0800

    - 3.91.1_D  중산회풍대상  창시7258화상대화결합  sc201cs color isp v2.1.0.49

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -574,7 +574,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_JIARUI_UAC                    365
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_HW7258_TUYA_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_IR_JIGAO
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -2657,8 +2657,8 @@ enum E_Baud_Rate
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_IR_JIGAO)
 
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.91.0_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.91.0_D"
+#define DEVICE_FIRMWARE_VERSION             "3.91.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.91.1_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
@@ -2668,7 +2668,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v47
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v49
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
 #undef CONFIG_USB_HS
