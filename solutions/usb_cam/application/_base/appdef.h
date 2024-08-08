82731ac81c4e73ae5149583dba73a097df87b856
kzh <kevin9046@outlook.com>
2024-08-08 15:05:39 +0800

    - 3.101.0_D  창시도아화상대화결합  3.62.8_D에서 시작하여 audio wMaxPacketSize를 16으로 수정하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -573,9 +573,10 @@ enum E_Baud_Rate
 #define FRM_DBS3M_BK7256_UAC                    363
 #define FRM_DBS3M_AP7258_UAC                    364
 #define FRM_DBS3M_JIARUI_UAC                    365
+#define FRM_DBS3M_CS_TUYA_UAC                   366
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_AJISHI_CHANGSI_MODE
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_CS_TUYA_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -3557,6 +3558,50 @@ odd version: use_whiteled = 1
 #endif // USE_WHITE_LED
 
 //----------------------------------------------------------
+#elif (FRM_PRODUCT_TYPE == FRM_DBS3M_CS_TUYA_UAC)
+
+// 3.1.5_D
+#define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
+#define DEVICE_FIRMWARE_VERSION             "3.101.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.101.0_D"
+
+#undef UVC_RES_DEFINE
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
+                                            {2, 864, 480, 30, 0, 10240}, \
+                                            {3, 800, 480, 30, 0, 10240}, \
+                                            {4, 640, 480, 30, 0, 6144},
+#undef USE_WHITE_LED
+#define USE_WHITE_LED                       0
+#undef DEFAULT_ISP_BIN_VER
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v49
+#undef SPECIFIC_LOG_PRINT
+#define SPECIFIC_LOG_PRINT                  1
+#undef UVC_USBD_PRINT
+#define UVC_USBD_PRINT                      1
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
+#undef CONFIG_DWC2_VERSION
+#define CONFIG_DWC2_VERSION                 1
+#undef UAC_EP_WMAXPCKT_SIZE
+#define UAC_EP_WMAXPCKT_SIZE                16  //audio ep wMaxPacketSize
+
+#if (USE_WHITE_LED == 0)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_SEMI
+#define UVC_CLR2IR_THR4ISP                  (-50)
+#undef UVC_DARK_WATCH_COUNTER
+#define UVC_DARK_WATCH_COUNTER              10
+
+#elif (USE_WHITE_LED == 1)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_DEFAULT
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#define UVC_CLR2IR_THR4ENGINE               (-30)
+
+#else // USE_WHITE_LED
+#error "USE_WHITE_LED must be 0 or 1."
+#endif // USE_WHITE_LED
+
 #endif // FRM_PRODUCT_TYPE
 
 #if (UVC_MAX_WIDTH < UVC_WIDTH && UVC_MAX_HEIGHT < UVC_HEIGHT)
