ee81b75ce3299d9f4498ecdff585df7e2cad6ffa
kzh <kevin9046@outlook.com>
2024-08-15 14:12:21 +0800

    - 3.101.0.2_D  +uvc 480x854

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -577,7 +577,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_CS_TUYA_UAC                   366
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LS7258_IR_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_CS_TUYA_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -3581,14 +3581,15 @@ odd version: use_whiteled = 1
 
 // 3.1.5_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.101.0.1_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.101.0.1_D"
+#define DEVICE_FIRMWARE_VERSION             "3.101.0.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.101.0.2_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 480, 864, 30, UVC_ROTATION_0 + 1, 8192}, \
                                             {2, 480, 800, 30, UVC_ROTATION_0 + 1, 8192}, \
-                                            {3, 480, 640, 30, UVC_ROTATION_0 + 1, 6144}, \
-                                            {4, 480, 320, 30, 0, 6144},
+                                            {3, 480, 854, 30, UVC_ROTATION_0 + 1, 8192}, \
+                                            {4, 480, 640, 30, UVC_ROTATION_0 + 1, 6144}, \
+                                            {5, 480, 320, 30, 0, 6144},
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
