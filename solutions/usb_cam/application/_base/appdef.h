7c732b46ba4efa965fff064a4055eda41365f2e8
kzh <kevin9046@outlook.com>
2024-08-17 12:52:56 +0800

    - 3.61.2.1_D  일반,백색레드 쓰지 않음(3.9.2_D)  현시효과확인을 위한 시험펌웨어  sc201cs color isp v2.1.0.54 - sc201cs color isp v2.1.0.54 절강청송에서 제기된 요구에 따라 수정함. 변두리부분의 밝기가 어두운 문제를 개선, 밝기를 올림. 색조를 노란색쪽으로 기울임.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -159,6 +159,7 @@ enum E_Baud_Rate
 #define ISP_BIN_VER_21v49           2149
 #define ISP_BIN_VER_21v50           2150
 #define ISP_BIN_VER_21v52           2152
+#define ISP_BIN_VER_21v54           2154
 #define ISP_BIN_VER_211v0           21100 //v2.1.1.0
 #define ISP_BIN_VER_22v0            20
 #define ISP_BIN_VER_301v9           21
@@ -578,7 +579,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_CS_TUYA_UAC                   366
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_KEXIONG_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LANCENS_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -986,8 +987,8 @@ enum E_Baud_Rate
 
 // 3.9.2_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.61.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.61.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.61.2.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.61.2.1_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
@@ -997,7 +998,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v49
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v54
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
 #undef UVC_USBD_PRINT
