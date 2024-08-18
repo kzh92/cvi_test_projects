dc996c7c737eb0c62bbac15ce3d546f4a4d5b400
kzh <kevin9046@outlook.com>
2024-08-18 11:21:01 +0800

    - 3.63.6_D  절강박극대상  웅매S3화상대화모듈련동

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -579,7 +579,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_CS_TUYA_UAC                   366
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LS7258_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_XIONGMAI_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -2414,8 +2414,8 @@ enum E_Baud_Rate
 
 //3.44.9_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.63.5_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.63.5_D"
+#define DEVICE_FIRMWARE_VERSION             "3.63.6_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.63.6_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
