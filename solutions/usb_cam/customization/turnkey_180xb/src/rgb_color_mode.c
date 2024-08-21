c4fe8219bf4fc6669084d88f22c4e4a8ef411ff1
kzh <kevin9046@outlook.com>
2024-08-21 14:19:55 +0800

    - sc201cs color isp v2.1.0.55 절강청송에서 제기한 요구에 따라 수정함. 밝기가 2.1.0.54에 비하여 약간 밝고 색조를 더 노란색쪽으로 수정함. Sharpen을 더 올림.

--- solutions/usb_cam/customization/turnkey_180xb/src/rgb_color_mode.c
+++ solutions/usb_cam/customization/turnkey_180xb/src/rgb_color_mode.c
@@ -94,6 +94,8 @@
 #include "rgb_color_v2.1.0.52.inc"
 #elif (DEFAULT_ISP_BIN_VER == ISP_BIN_VER_21v54)
 #include "rgb_color_v2.1.0.54.inc"
+#elif (DEFAULT_ISP_BIN_VER == ISP_BIN_VER_21v55)
+#include "rgb_color_v2.1.0.55.inc"
 #elif (DEFAULT_ISP_BIN_VER == ISP_BIN_VER_211v0)
 #include "rgb_color_v2.1.1.0.inc"
 #elif (DEFAULT_ISP_BIN_VER == ISP_BIN_VER_22v0)
