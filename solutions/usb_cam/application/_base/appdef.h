f3e75d142a504aacf82a9b20704c4d4c2e7ffe7b
kzh <kevin9046@outlook.com>
2024-08-26 22:21:14 +0800

    - 3.78.2_D  동관동흔조종기판+일반화상대화(3.61.3_D)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -582,7 +582,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_RENT                          367
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_D10A_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_YIHONG_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -3025,33 +3025,45 @@ enum E_Baud_Rate
 
 // 3.62.1_D + USE_TONGXIN_PROTO
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.78.0_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.78.0_D"
+#define DEVICE_FIRMWARE_VERSION             "3.78.2_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.78.2_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
                                             {2, 864, 480, 30, 0, 10240}, \
                                             {3, 800, 480, 30, 0, 10240}, \
                                             {4, 640, 480, 30, 0, 6144},
-#undef USE_3M_MODE
-#define USE_3M_MODE                         U3M_SEMI
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v11
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v49
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
-#define UVC_CLR2IR_THR4ISP                  (-50)
 #undef UVC_USBD_PRINT
 #define UVC_USBD_PRINT                      1
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
-#undef UVC_DARK_WATCH_COUNTER
-#define UVC_DARK_WATCH_COUNTER              10
 #undef UAC_SPEAKER_VOL
 #define UAC_SPEAKER_VOL                     6 // 0 ~ 32
 #undef USE_TONGXIN_PROTO
 #define USE_TONGXIN_PROTO                   1
+
+#if (USE_WHITE_LED == 0)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_SEMI
+#define UVC_CLR2IR_THR4ISP                  (-50)
+#undef UVC_DARK_WATCH_COUNTER
+#define UVC_DARK_WATCH_COUNTER              10
+
+#elif (USE_WHITE_LED == 1)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_DEFAULT
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#define UVC_CLR2IR_THR4ENGINE               (-30)
+
+#else // USE_WHITE_LED
+#error "USE_WHITE_LED must be 0 or 1."
+#endif // USE_WHITE_LED
 
 //----------------------------------------------------------
 #elif (FRM_PRODUCT_TYPE == FRM_DBS3M_OKD_IR_UAC)
