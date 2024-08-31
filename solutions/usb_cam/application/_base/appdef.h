7c6418561b65e3f29d0d1491a4e4b41e31250fc6
kzh <kevin9046@outlook.com>
2024-08-31 14:39:22 +0800

    - 3.75.8.1_D  웅매7258,람성7258,호위7258화상대화련동  3.01AC  시험용,화상대화현시방향을 180도 회전하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -584,7 +584,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_RENT                          367
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_HONGLI_MODE
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LS7258_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -2278,8 +2278,8 @@ enum E_Baud_Rate
 
 // 3.41.0
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.75.8_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.75.8_D"
+#define DEVICE_FIRMWARE_VERSION             "3.75.8.1_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.75.8.1_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 864, 480, 30, 0, 8192}, \
@@ -2321,6 +2321,8 @@ enum E_Baud_Rate
 #define UVC_MAX_HEIGHT                      480
 #undef UVC_DELAY_BEFORE_START
 #define UVC_DELAY_BEFORE_START              700
+#undef DEFAULT_UVC_DIR
+#define DEFAULT_UVC_DIR                     UVC_ROTATION_270
 
 #if (USE_WHITE_LED == 0)
 #undef USE_3M_MODE
