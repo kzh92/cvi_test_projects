5cb21f98b9edcf46055b0698a6f6a89f169ef0ad
kzh <kevin9046@outlook.com>
2024-09-13 18:04:50 +0800

    - 3.108.0_D  람성7258화상대화모듈련동(3.75.8_D에서 화질이 낮다고 하므로 따로 작성함)  3.75.10_D판본에서 YUV422관련한 설정들을 막았음.  해상도설정은 3.61.4_D/3.73.4_D판본에 맞추었음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -587,9 +587,10 @@ enum E_Baud_Rate
 #define FRM_DBS3M_QINGSONG                      368
 #define FRM_DBS3M_JUNLI_UAC                     369
 #define FRM_DBS3M_LS7258_UVC270_UAC             370
+#define FRM_DBS3M_LC7258_UAC                    371
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LS7258_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LC7258_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -3892,6 +3893,54 @@ odd version: use_whiteled = 1
 #define UVC_DELAY_BEFORE_START              700
 #undef DEFAULT_UVC_DIR
 #define DEFAULT_UVC_DIR                     UVC_ROTATION_270
+
+#if (USE_WHITE_LED == 0)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_SEMI
+#define UVC_CLR2IR_THR4ISP                  (-50)
+#undef UVC_DARK_WATCH_COUNTER
+#define UVC_DARK_WATCH_COUNTER              10
+
+#elif (USE_WHITE_LED == 1)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_DEFAULT
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#define UVC_CLR2IR_THR4ENGINE               (-30)
+
+#else // USE_WHITE_LED
+#error "USE_WHITE_LED must be 0 or 1."
+#endif // USE_WHITE_LED
+
+//----------------------------------------------------------
+#elif (FRM_PRODUCT_TYPE == FRM_DBS3M_LC7258_UAC)
+
+#define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
+#define DEVICE_FIRMWARE_VERSION             "3.108.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.108.0_D"
+
+#undef UVC_RES_DEFINE
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
+                                            {2, 864, 480, 30, 0, 10240}, \
+                                            {3, 800, 480, 30, 0, 8192}, \
+                                            {4, 640, 480, 30, 0, 6144},
+#undef USE_WHITE_LED
+#define USE_WHITE_LED                       0
+#undef DEFAULT_ISP_BIN_VER
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v60
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
+#undef SPECIFIC_LOG_PRINT
+#define SPECIFIC_LOG_PRINT                  1
+#undef UVC_USBD_PRINT
+#define UVC_USBD_PRINT                      1
+#undef CONFIG_USB_HS
+#define CONFIG_USB_HS                       0
+#undef CONFIG_DWC2_VERSION
+#define CONFIG_DWC2_VERSION                 1
+#undef UAC_SPK_NR_USE
+#define UAC_SPK_NR_USE                      1
+#undef UVC_DELAY_BEFORE_START
+#define UVC_DELAY_BEFORE_START              700
 
 #if (USE_WHITE_LED == 0)
 #undef USE_3M_MODE
