3f2a9102256b0b5e037ac0f25cb24e6d1bfd0865
kzh <kevin9046@outlook.com>
2024-10-14 23:10:40 +0800

    - 3.62.16_D  절강명개,심전그리웰대상용  S3화상대화모듈용

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -599,15 +599,15 @@ enum E_Baud_Rate
 #define FRM_DBS3M_TX_LS7258_UAC                 374
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_TX_LS7258_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_YIHE_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
 
 // 3.1.5_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.62.14_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.62.14_D"
+#define DEVICE_FIRMWARE_VERSION             "3.62.16_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.62.16_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
