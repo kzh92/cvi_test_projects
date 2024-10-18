4dd08309902e8a068e070ace399e20af17681010
kzh <kevin9046@outlook.com>
2024-10-18 16:34:17 +0800

    - 3.115.0_D  3.62.16_D에서 시작.  UAC Mono 16K

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -487,9 +487,10 @@ enum E_Baud_Rate
 #define FRM_DBS3M_LC7916_UAC                    373
 #define FRM_DBS3M_TX_LS7258_UAC                 374
 #define FRM_DBS3M_UVC_IRLED_ON                  375
+#define FRM_DBS3M_YIHE_UAC16K                   376
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_UVC_IRLED_ON
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_YIHE_UAC16K
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -4089,6 +4090,54 @@ odd version: use_whiteled = 1
 #error "USE_WHITE_LED must be 0 or 1."
 #endif // USE_WHITE_LED
 
+//----------------------------------------------------------
+#elif (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC16K)
+
+// 3.62.16_D
+#define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
+#define DEVICE_FIRMWARE_VERSION             "3.115.0_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.115.0_D"
+
+#undef UVC_RES_DEFINE
+#define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
+                                            {2, 864, 480, 30, 0, 10240}, \
+                                            {3, 800, 480, 30, 0, 10240}, \
+                                            {4, 640, 480, 30, 0, 6144},
+#undef USE_WHITE_LED
+#define USE_WHITE_LED                       0
+#undef DEFAULT_ISP_BIN_VER
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v60
+#undef SPECIFIC_LOG_PRINT
+#define SPECIFIC_LOG_PRINT                  1
+#undef UVC_USBD_PRINT
+#define UVC_USBD_PRINT                      1
+#undef USE_USB_EP_ERR_FIX_MODE
+#define USE_USB_EP_ERR_FIX_MODE             1
+#undef UAC_SPK_NR_USE
+#define UAC_SPK_NR_USE                      2
+#undef UVC_CROP_RESIZE
+#define UVC_CROP_RESIZE                     0.875
+#undef UAC_SPEAKER_VOL
+#define UAC_SPEAKER_VOL                     32 // 0 ~ 32
+#undef UAC_SAMPLE_RATE
+#define UAC_SAMPLE_RATE                     16000
+
+#if (USE_WHITE_LED == 0)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_SEMI
+#define UVC_CLR2IR_THR4ISP                  (-50)
+#undef UVC_DARK_WATCH_COUNTER
+#define UVC_DARK_WATCH_COUNTER              10
+
+#elif (USE_WHITE_LED == 1)
+#undef USE_3M_MODE
+#define USE_3M_MODE                         U3M_DEFAULT
+#define UVC_CLR2IR_THR4ISP                  (-200) //threshold value for turning white led on.
+#define UVC_CLR2IR_THR4ENGINE               (-30)
+
+#else // USE_WHITE_LED
+#error "USE_WHITE_LED must be 0 or 1."
+#endif // USE_WHITE_LED
 //----------------------------------------------------------
 
 #endif // FRM_PRODUCT_TYPE
