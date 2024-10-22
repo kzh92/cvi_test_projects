e760ccfd1bd288f95a01c71aa0720450a4d7efa5
kzh <kevin9046@outlook.com>
2024-10-22 14:42:34 +0800

    - 3.61.6.4_D 3.61.5_D판본에서 시작하여 광주적고에서 제기된 요구사항에 따라 밝기를 약간 낮춘 색카메라현시효과 v2.1.0.65를 결합하였음. 이전 결합에 문제가 있어 다시 묶었음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -492,7 +492,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_JIJIA_UAC                     377
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_YIHE2_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_LANCENS_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -900,8 +900,8 @@ enum E_Baud_Rate
 
 // 3.9.2_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.61.6.3_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.61.6.3_D"
+#define DEVICE_FIRMWARE_VERSION             "3.61.6.4_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.61.6.4_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
