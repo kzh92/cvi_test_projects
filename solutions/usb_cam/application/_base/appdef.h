45bae1740526926b8688ec069e55010cb22cab15
kzh <kevin9046@outlook.com>
2024-10-26 22:58:34 +0800

    - 3.78.4_D  동관동흔조종기판+일반화상대화(3.61.3_D)

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -493,7 +493,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_JIJIA_UAC                     377
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_FANHAI_MODE
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_YIHONG_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -2931,8 +2931,8 @@ enum E_Baud_Rate
 
 // 3.62.1_D + USE_TONGXIN_PROTO
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.78.2_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.78.2_D"
+#define DEVICE_FIRMWARE_VERSION             "3.78.4_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.78.4_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 30, 0}, \
