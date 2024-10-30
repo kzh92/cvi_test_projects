22a566ad8967ed3fb1bfb2622527fd2edece8f9e
kzh <kevin9046@outlook.com>
2024-10-30 08:49:45 +0800

    -(공통)적외선화상대화출력에서 bitrate가 4096보다 클때에만 절반으로 떨구어 출력하게 하였음.  4096인 해상도의 화질이 나빠질수있어 수정하였음.

--- components/cvi_platform/protocol/av_composite/src/av_comp_app.c
+++ components/cvi_platform/protocol/av_composite/src/av_comp_app.c
@@ -391,7 +391,7 @@ int uvc_media_update(){
 	if (g_xSS.iUvcBitrate > 0 && enType == PT_MJPEG)
 	{
 		pstVencCfg->pstVencChnCfg[UVC_VENC_CHN].stRcParam.u16BitRate = g_xSS.iUvcBitrate;
-		if (g_xSS.iUvcSensor != DEFAULT_SNR4UVC && g_xSS.iUvcBitrate >= 4096)
+		if (g_xSS.iUvcSensor != DEFAULT_SNR4UVC && g_xSS.iUvcBitrate > 4096)
 		{
 			pstVencCfg->pstVencChnCfg[UVC_VENC_CHN].stRcParam.u16BitRate = g_xSS.iUvcBitrate / 2;
 		}
