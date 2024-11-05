1a1687a77a29c0a930cb6a36739ccad450dae55e
kzh <kevin9046@outlook.com>
2024-11-06 00:08:31 +0800

    - 3.100.6_D  심전기하  동흔조종기판+창시기유화상대화  색카메라화상의 화질을 개선한 ISP조종결과물 v2.1.0.70를 결합하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -499,7 +499,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_7258_HSM_UAC                  378
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_YIHONG_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_TX_PROTO_IR
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -1954,8 +1954,8 @@ enum E_Baud_Rate
 
 // 3.33.1_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.100.4_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.100.4_D"
+#define DEVICE_FIRMWARE_VERSION             "3.100.6_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.100.6_D"
 
 #undef UVC_RES_DEFINE
 #define UVC_RES_DEFINE                      {1, 1280, 720, 15, 0, 20480}, \
@@ -1966,7 +1966,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v60
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v70
 #undef USE_USB_EP_ERR_FIX_MODE
 #define USE_USB_EP_ERR_FIX_MODE             1
 #undef SPECIFIC_LOG_PRINT
