2409bcab8d350c8fca0b71667825e7bdadeb551d
kzh <kevin9046@outlook.com>
2024-11-11 10:13:23 +0800

    - 3.83.6_D  중산소피만대상  흠홍가(부시손바닥규약)+창시도아  색카메라화상의 화질을 개선한 ISP조종결과물 v2.1.0.70를 결합하였음.

--- solutions/usb_cam/application/_base/appdef.h
+++ solutions/usb_cam/application/_base/appdef.h
@@ -501,7 +501,7 @@ enum E_Baud_Rate
 #define FRM_DBS3M_KFZ_UAC                       379
 #define FRM_DBS3M_D10A_UAC                      400
 
-#define FRM_PRODUCT_TYPE                        FRM_DBS3M_KEXIONG_UAC
+#define FRM_PRODUCT_TYPE                        FRM_DBS3M_SUOFEIWAN_UAC
 
 //----------------------------------------------------------
 #if (FRM_PRODUCT_TYPE == FRM_DBS3M_YIHE_UAC)
@@ -3110,8 +3110,8 @@ enum E_Baud_Rate
 
 // 3.61.1_D
 #define DEVICE_MODEL_NUM                    "BIOAT-FM-175"
-#define DEVICE_FIRMWARE_VERSION             "3.83.4_D"
-#define DEVICE_FIRMWARE_VERSION_INNER       "3.83.4_D"
+#define DEVICE_FIRMWARE_VERSION             "3.83.6_D"
+#define DEVICE_FIRMWARE_VERSION_INNER       "3.83.6_D"
 
 #undef DEFAULT_CHIP_TYPE
 #define DEFAULT_CHIP_TYPE                   MY_CHIP_D10
@@ -3123,7 +3123,7 @@ enum E_Baud_Rate
 #undef USE_WHITE_LED
 #define USE_WHITE_LED                       0
 #undef DEFAULT_ISP_BIN_VER
-#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v49
+#define DEFAULT_ISP_BIN_VER                 ISP_BIN_VER_21v70
 #undef SPECIFIC_LOG_PRINT
 #define SPECIFIC_LOG_PRINT                  1
 #undef UVC_USBD_PRINT
